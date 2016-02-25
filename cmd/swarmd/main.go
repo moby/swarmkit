0cc38e9c4b5bcf9fb5843b3cea2c68165bc4be17
Stephen J Day <stephen.day@docker.com>
2016-02-24 21:31:54 -0800

    cmd: add common version command for binaries
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- cmd/swarmd/main.go
+++ cmd/swarmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/docker/swarm-v2/version"
+import (
+	"fmt"
+	"os"
+
+	"github.com/docker/swarm-v2/version"
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	version.PrintVersion()
+	if err := mainCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+var (
+	mainCmd = &cobra.Command{
+		Use:   os.Args[0],
+		Short: "Run a swarm control process",
+	}
+)
+
+func init() {
+	mainCmd.AddCommand(version.Cmd)
 }
