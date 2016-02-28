4e2be701e80fdb59d13cf4e004607e5c38512c47
Andrea Luzzardi <aluzzardi@gmail.com>
2016-02-27 17:45:13 -0800

    swarmctl: Minimal job ls implementation.
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cmd/swarmctl/job/ls.go
+++ cmd/swarmctl/job/ls.go
@@ -1,12 +1,29 @@
 package job
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/docker/swarm-v2/api"
+	"github.com/docker/swarm-v2/cmd/swarmctl/common"
+	"github.com/spf13/cobra"
+)
 
 var (
 	lsCmd = &cobra.Command{
 		Use:   "ls",
 		Short: "List jobs",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			c, err := common.Dial(cmd)
+			if err != nil {
+				return err
+			}
+			r, err := c.ListJobs(common.Context(cmd), &api.ListJobsRequest{})
+			if err != nil {
+				return err
+			}
+			for _, j := range r.Jobs {
+				fmt.Println(j.ID)
+			}
 			return nil
 		},
 	}
