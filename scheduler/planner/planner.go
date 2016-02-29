00d2918707f2a597341382920a721f344e7ad813
Aaron Lehmann <aaron.lehmann@docker.com>
2016-02-29 14:49:50 -0800

    Update planner to use transactions properly
    This makes the planner batch scheduling decisions in a single
    transaction. If the transaction fails, the locally-maintained list of
    unassigned tasks is left unaltered.
    
    Add a unit test for the planner.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- scheduler/planner/planner.go
+++ scheduler/planner/planner.go
@@ -2,7 +2,6 @@ package planner
 
 import (
 	"container/list"
-	"errors"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/swarm-v2/api"
@@ -11,8 +10,8 @@ import (
 
 // A Planner assigns tasks to nodes.
 type Planner struct {
-	store state.WatchableStore
-	queue *list.List
+	store           state.WatchableStore
+	unassignedTasks *list.List
 
 	// stopChan signals to the state machine to stop running
 	stopChan chan struct{}
@@ -23,83 +22,71 @@ type Planner struct {
 // New creates a new planner.
 func New(store state.WatchableStore) *Planner {
 	return &Planner{
-		store:    store,
-		queue:    list.New(),
-		stopChan: make(chan struct{}),
-		doneChan: make(chan struct{}),
+		store:           store,
+		unassignedTasks: list.New(),
+		stopChan:        make(chan struct{}),
+		doneChan:        make(chan struct{}),
 	}
 }
 
-// Run is the planner event loop.
-func (p *Planner) Run() {
-	defer close(p.doneChan)
-
-	// Watch for tasks with no NodeID
-	watchQueue := p.store.WatchQueue()
-	unassignedTasks := state.Watch(watchQueue,
-		state.EventCreateTask{Task: &api.Task{NodeID: ""},
-			Checks: []state.TaskCheckFunc{state.TaskCheckNodeID}},
-		state.EventUpdateTask{Task: &api.Task{NodeID: ""},
-			Checks: []state.TaskCheckFunc{state.TaskCheckNodeID}})
-
-	// Watch for valid nodes
-	nodeChanges := state.Watch(watchQueue,
-		state.EventCreateNode{Node: &api.Node{Status: api.NodeStatus{State: api.NodeStatus_READY}},
-			Checks: []state.NodeCheckFunc{state.NodeCheckStatus}},
-		state.EventUpdateNode{Node: &api.Node{Status: api.NodeStatus{State: api.NodeStatus_READY}},
-			Checks: []state.NodeCheckFunc{state.NodeCheckStatus}})
-
-	// Queue all unassigned tasks before watching for changes.
-	err := p.store.View(func(tx state.ReadTx) error {
-		tasks, err := tx.Tasks().Find(state.ByNodeID(""))
-		if err != nil {
-			log.Errorf("Error finding unassigned tasks: %v", err)
-			return nil
-		}
-		for _, t := range tasks {
+func (p *Planner) setupTasksList(tx state.ReadTx) error {
+	tasks, err := tx.Tasks().Find(state.All)
+	if err != nil {
+		return err
+	}
+	for _, t := range tasks {
+		if t.NodeID == "" {
 			log.Infof("Queueing %#v", t)
 			p.enqueue(t)
 		}
-		return nil
-	})
-	if err != nil {
-		log.Errorf("Error in transaction: %v", err)
 	}
+
+	return nil
+}
+
+// Run is the planner event loop.
+func (p *Planner) Run() error {
+	defer close(p.doneChan)
+
+	updates := p.store.WatchQueue().Watch()
+	defer p.store.WatchQueue().StopWatch(updates)
+
+	err := p.store.View(p.setupTasksList)
+	if err != nil {
+		log.Errorf("could not snapshot store: %v", err)
+		return err
+	}
+
+	// Queue all unassigned tasks before processing changes.
 	p.tick()
 
-	unassignedTasksClosed := false
-	nodeChangesClosed := false
+	pendingChanges := 0
 
 	// Watch for changes.
 	for {
-		if unassignedTasksClosed && nodeChangesClosed {
-			return
-		}
-
 		select {
-		case event, ok := <-unassignedTasks:
-			if !ok {
-				unassignedTasksClosed = true
-				continue
-			}
-			var task *api.Task
+		case event := <-updates:
 			switch v := event.Payload.(type) {
 			case state.EventCreateTask:
-				task = v.Task
+				pendingChanges += p.createTask(v.Task)
 			case state.EventUpdateTask:
-				task = v.Task
+				pendingChanges += p.createTask(v.Task)
+			case state.EventCreateNode:
+				if v.Node.Status.State == api.NodeStatus_READY {
+					pendingChanges++
+				}
+			case state.EventUpdateNode:
+				if v.Node.Status.State == api.NodeStatus_READY {
+					pendingChanges++
+				}
+			case state.EventCommit:
+				if pendingChanges > 0 {
+					p.tick()
+					pendingChanges = 0
+				}
 			}
-			p.enqueue(task)
-			p.tick()
-		case _, ok := <-nodeChanges:
-			if !ok {
-				nodeChangesClosed = true
-				continue
-			}
-			p.tick()
 		case <-p.stopChan:
-			watchQueue.StopWatch(unassignedTasks)
-			watchQueue.StopWatch(nodeChanges)
+			return nil
 		}
 	}
 }
@@ -112,70 +99,95 @@ func (p *Planner) Stop() {
 
 // enqueue queues a task for scheduling.
 func (p *Planner) enqueue(t *api.Task) {
-	p.queue.PushBack(t)
+	p.unassignedTasks.PushBack(t)
+}
+
+func (p *Planner) createTask(t *api.Task) int {
+	if t.NodeID == "" {
+		// unassigned task
+		p.enqueue(t)
+		return 1
+	}
+	return 0
 }
 
 // tick attempts to schedule the queue.
 func (p *Planner) tick() {
-	var next *list.Element
-	for e := p.queue.Front(); e != nil; e = next {
-		next = e.Next()
-		t := e.Value.(*api.Task)
-		if p.scheduleTask(t) {
-			p.queue.Remove(e)
+	nextBatch := list.New()
+
+	// TODO(aaronl): Ideally, we would make scheduling decisions outside
+	// of an Update callback, since Update blocks other writes to the
+	// store. The current approach of making the decisions inside Update
+	// is done to keep the store simple. Eventually, we may want to break
+	// this up into a View where the decisions are made, and an Update that
+	// applies them. This will require keeping local state to keep track of
+	// allocations as they are made, since the store itself can't be
+	// changed through View.
+	err := p.store.Update(func(tx state.Tx) error {
+		nodes, err := tx.Nodes().Find(state.All)
+		if err != nil {
+			return err
 		}
+
+		var next *list.Element
+		for e := p.unassignedTasks.Front(); e != nil; e = next {
+			next = e.Next()
+			t := e.Value.(*api.Task)
+			if newT := p.scheduleTask(tx, nodes, *t); newT == nil {
+				// scheduling failed; keep this task in the list
+				nextBatch.PushBack(t)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Errorf("Error in transaction: %v", err)
+
+		// leave unassignedTasks list in place
+	} else {
+		p.unassignedTasks = nextBatch
 	}
 }
 
 // scheduleTask schedules a single task.
-func (p *Planner) scheduleTask(t *api.Task) bool {
-	err := p.store.Update(func(tx state.Tx) error {
-		node := p.selectNodeForTask(t, tx)
-		if node == nil {
-			err := errors.New("No nodes available to assign tasks to")
-			log.Info(err)
-			return err
-		}
-
-		log.Infof("Assigning task %s to node %s", t.ID, node.ID)
-		t.NodeID = node.ID
-		t.Status.State = api.TaskStatus_ASSIGNED
-		if err := tx.Tasks().Update(t); err != nil {
-			log.Error(err)
-			return err
-		}
+func (p *Planner) scheduleTask(tx state.Tx, nodes []*api.Node, t api.Task) *api.Task {
+	node := p.selectNodeForTask(tx, nodes, &t)
+	if node == nil {
+		log.Info("No nodes available to assign tasks to")
 		return nil
-	})
-	if err != nil {
-		return false
 	}
-	return true
+
+	log.Infof("Assigning task %s to node %s", t.ID, node.ID)
+	t.NodeID = node.ID
+	t.Status = &api.TaskStatus{State: api.TaskStatus_ASSIGNED}
+	if err := tx.Tasks().Update(&t); err != nil {
+		log.Error(err)
+		return nil
+	}
+	return &t
 }
 
 // selectNodeForTask is a naive scheduler. Will select a ready, non-drained
 // node with the fewer number of tasks already running.
-func (p *Planner) selectNodeForTask(t *api.Task, tx state.Tx) *api.Node {
+func (p *Planner) selectNodeForTask(tx state.Tx, nodes []*api.Node, t *api.Task) *api.Node {
 	var target *api.Node
 	targetTasks := 0
 
-	nodes, err := tx.Nodes().Find(state.All)
-	if err != nil {
-		log.Errorf("Error listing nodes: %v", err)
-		return nil
-	}
-
 	for _, n := range nodes {
-		if n.Status.State == api.NodeStatus_READY /*&& !n.Drained*/ {
-			tasks, err := tx.Tasks().Find(state.ByNodeID(n.ID))
-			if err != nil {
-				log.Errorf("Error selecting tasks by node: %v", err)
-				continue
-			}
-			nodeTasks := len(tasks)
-			if target == nil || nodeTasks < targetTasks {
-				target = n
-				targetTasks = nodeTasks
-			}
+		if n.Status.State != api.NodeStatus_READY {
+			continue
+		}
+
+		tasks, err := tx.Tasks().Find(state.ByNodeID(n.ID))
+		if err != nil {
+			log.Errorf("Error selecting tasks by node: %v", err)
+			continue
+		}
+		nodeTasks := len(tasks)
+		if target == nil || nodeTasks < targetTasks {
+			target = n
+			targetTasks = nodeTasks
 		}
 	}
 
---
+++ scheduler/planner/planner_test.go
@@ -0,0 +1,397 @@
+package planner
+
+import (
+	"testing"
+	"time"
+
+	"github.com/docker/swarm-v2/api"
+	"github.com/docker/swarm-v2/state"
+	"github.com/docker/swarm-v2/watch"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlanner(t *testing.T) {
+	initialNodeSet := []*api.Node{
+		{
+			ID: "id1",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name1",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		},
+		{
+			ID: "id2",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name2",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		},
+		{
+			ID: "id3",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name2",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		},
+	}
+
+	initialTaskSet := []*api.Task{
+		{
+			ID: "id1",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name1",
+				},
+			},
+			NodeID: initialNodeSet[0].ID,
+		},
+		{
+			ID: "id2",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name2",
+				},
+			},
+		},
+		{
+			ID: "id3",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name2",
+				},
+			},
+		},
+	}
+
+	store := state.NewMemoryStore()
+	assert.NotNil(t, store)
+
+	err := store.Update(func(tx state.Tx) error {
+		// Prepoulate nodes
+		for _, n := range initialNodeSet {
+			assert.NoError(t, tx.Nodes().Create(n))
+		}
+
+		// Prepopulate tasks
+		for _, task := range initialTaskSet {
+			assert.NoError(t, tx.Tasks().Create(task))
+		}
+		return nil
+	})
+	assert.NoError(t, err)
+
+	planner := New(store)
+
+	watch := state.Watch(store.WatchQueue(), state.EventUpdateTask{})
+	defer store.WatchQueue().StopWatch(watch)
+
+	go func() {
+		assert.NoError(t, planner.Run())
+	}()
+
+	assignment1 := watchAssignment(t, watch)
+	// must assign to id2 or id3 since id1 already has a task
+	assert.Regexp(t, assignment1.NodeID, "(id2|id3)")
+
+	assignment2 := watchAssignment(t, watch)
+	// must assign to id2 or id3 since id1 already has a task
+	if assignment1.NodeID == "id2" {
+		assert.Equal(t, assignment2.NodeID, "id3")
+	} else {
+		assert.Equal(t, assignment2.NodeID, "id2")
+	}
+
+	err = store.Update(func(tx state.Tx) error {
+		// Delete the task associated with node 1 so it's now the most lightly
+		// loaded node.
+		assert.NoError(t, tx.Tasks().Delete("id1"))
+
+		// Create a new task. It should get assigned to id1.
+		t4 := &api.Task{
+			ID: "id4",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name4",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Create(t4))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignment3 := watchAssignment(t, watch)
+	assert.Equal(t, assignment3.NodeID, "id1")
+
+	// Update a task to make it unassigned. It should get assigned by the
+	// planner.
+	err = store.Update(func(tx state.Tx) error {
+		// Remove assignment from task id4. It should get assigned
+		// to node id1.
+		t4 := &api.Task{
+			ID: "id4",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name4",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Update(t4))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Discard first UpdateTask - that's our own UpdateTask
+	watchAssignment(t, watch)
+	assignment4 := watchAssignment(t, watch)
+	assert.Equal(t, assignment4.NodeID, "id1")
+
+	err = store.Update(func(tx state.Tx) error {
+		// Create a ready node, then remove it. No tasks should ever
+		// be assigned to it.
+		node := &api.Node{
+			ID: "removednode",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "removednode",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_DOWN,
+			},
+		}
+		assert.NoError(t, tx.Nodes().Create(node))
+		assert.NoError(t, tx.Nodes().Delete(node.ID))
+
+		// Create an unassigned task.
+		task := &api.Task{
+			ID: "removednode",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "removednode",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Create(task))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignmentRemovedNode := watchAssignment(t, watch)
+	assert.NotEqual(t, assignmentRemovedNode.NodeID, "removednode")
+
+	err = store.Update(func(tx state.Tx) error {
+		// Create a ready node. It should be used for the next
+		// assignment.
+		n4 := &api.Node{
+			ID: "id4",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name4",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		}
+		assert.NoError(t, tx.Nodes().Create(n4))
+
+		// Create an unassigned task.
+		t5 := &api.Task{
+			ID: "id5",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name5",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Create(t5))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignment5 := watchAssignment(t, watch)
+	assert.Equal(t, assignment5.NodeID, "id4")
+
+	err = store.Update(func(tx state.Tx) error {
+		// Create a non-ready node. It should NOT be used for the next
+		// assignment.
+		n5 := &api.Node{
+			ID: "id5",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name5",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_DOWN,
+			},
+		}
+		assert.NoError(t, tx.Nodes().Create(n5))
+
+		// Create an unassigned task.
+		t6 := &api.Task{
+			ID: "id6",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name6",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Create(t6))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignment6 := watchAssignment(t, watch)
+	assert.NotEqual(t, assignment6.NodeID, "id5")
+
+	err = store.Update(func(tx state.Tx) error {
+		// Update node id5 to put it in the READY state.
+		n5 := &api.Node{
+			ID: "id5",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name5",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		}
+		assert.NoError(t, tx.Nodes().Update(n5))
+
+		// Create an unassigned task. Should be assigned to the
+		// now-ready node.
+		t7 := &api.Task{
+			ID: "id7",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name7",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Create(t7))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignment7 := watchAssignment(t, watch)
+	assert.Equal(t, assignment7.NodeID, "id5")
+
+	err = store.Update(func(tx state.Tx) error {
+		// Create a ready node, then immediately take it down. The next
+		// unassigned task should NOT be assigned to it.
+		n6 := &api.Node{
+			ID: "id6",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "name6",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		}
+		assert.NoError(t, tx.Nodes().Create(n6))
+		n6.Status.State = api.NodeStatus_DOWN
+		assert.NoError(t, tx.Nodes().Update(n6))
+
+		// Create an unassigned task.
+		t8 := &api.Task{
+			ID: "id8",
+			Spec: &api.JobSpec{
+				Meta: &api.Meta{
+					Name: "name8",
+				},
+			},
+		}
+		assert.NoError(t, tx.Tasks().Create(t8))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignment8 := watchAssignment(t, watch)
+	assert.NotEqual(t, assignment8.NodeID, "id6")
+
+	planner.Stop()
+}
+
+func TestPlannerNoReadyNodes(t *testing.T) {
+	initialTask := &api.Task{
+		ID: "id1",
+		Spec: &api.JobSpec{
+			Meta: &api.Meta{
+				Name: "name1",
+			},
+		},
+	}
+
+	store := state.NewMemoryStore()
+	assert.NotNil(t, store)
+
+	err := store.Update(func(tx state.Tx) error {
+		// Add initial task
+		assert.NoError(t, tx.Tasks().Create(initialTask))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	planner := New(store)
+
+	watch := state.Watch(store.WatchQueue(), state.EventUpdateTask{})
+	defer store.WatchQueue().StopWatch(watch)
+
+	go func() {
+		assert.NoError(t, planner.Run())
+	}()
+
+	err = store.Update(func(tx state.Tx) error {
+		// Create a ready node. The task should get assigned to this
+		// node.
+		node := &api.Node{
+			ID: "newnode",
+			Spec: &api.NodeSpec{
+				Meta: &api.Meta{
+					Name: "newnode",
+				},
+			},
+			Status: api.NodeStatus{
+				State: api.NodeStatus_READY,
+			},
+		}
+		assert.NoError(t, tx.Nodes().Create(node))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	assignment := watchAssignment(t, watch)
+	assert.Equal(t, assignment.NodeID, "newnode")
+
+	planner.Stop()
+}
+
+func watchAssignment(t *testing.T, watch chan watch.Event) *api.Task {
+	for {
+		select {
+		case event := <-watch:
+			if task, ok := event.Payload.(state.EventUpdateTask); ok {
+				return task.Task
+			}
+		case <-time.After(time.Second):
+			t.Fatalf("no task assignment")
+		}
+	}
+}
