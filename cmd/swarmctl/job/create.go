18327f7ef83ee79b690a2cf79317fc84f6f98bfb
Andrea Luzzardi <aluzzardi@gmail.com>
2016-03-02 21:51:43 -0800

    swarmctl: Initial support for `job create`
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

---
+++ cmd/swarmctl/job/create.go
@@ -0,0 +1,78 @@
+package job
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/docker/swarm-v2/api"
+	"github.com/docker/swarm-v2/cmd/swarmctl/common"
+	"github.com/spf13/cobra"
+)
+
+var (
+	createCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create a job",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// TODO(aluzzardi): Check cobra best practices for flags handling.
+			flags := cmd.Flags()
+			if !flags.Changed("name") || !flags.Changed("image") {
+				return errors.New("--name and --image are mandatory")
+			}
+			name, err := flags.GetString("name")
+			if err != nil {
+				return err
+			}
+			image, err := flags.GetString("image")
+			if err != nil {
+				return err
+			}
+			instances, err := flags.GetInt64("instances")
+			if err != nil {
+				return err
+			}
+
+			c, err := common.Dial(cmd)
+			if err != nil {
+				return err
+			}
+
+			// TODO(aluzzardi): `oneof` in the API are really ugly to use. We
+			// should reconsider having them embedded directly into the `main`
+			// type rather than into a nested type.
+			spec := &api.JobSpec{
+				Meta: &api.Meta{
+					Name: name,
+				},
+				Source: &api.Source{
+					Source: &api.Source_Image{
+						Image: &api.ImageSpec{
+							Reference: image,
+						},
+					},
+				},
+				Orchestration: &api.JobSpec_Orchestration{
+					Job: &api.JobSpec_Orchestration_Service{
+						Service: &api.JobSpec_ServiceJob{
+							Instances: instances,
+						},
+					},
+				},
+			}
+			r, err := c.CreateJob(common.Context(cmd), &api.CreateJobRequest{Spec: spec})
+			if err != nil {
+				return err
+			}
+			fmt.Println(r.Job.ID)
+			return nil
+		},
+	}
+)
+
+func init() {
+	createCmd.Flags().String("name", "", "Job name")
+	createCmd.Flags().String("image", "", "Image")
+	// TODO(aluzzardi): This should be called `service-instances` so that every
+	// orchestrator can have its own flag namespace.
+	createCmd.Flags().Int64("instances", 1, "Number of instances for the service Job")
+}
