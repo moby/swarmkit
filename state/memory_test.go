9878edb8414bbc7d912042780816e163d3c223fe
Aaron Lehmann <aaron.lehmann@docker.com>
2016-03-02 18:35:09 -0800

    Change memory store transactions to track in-progress changes
    This allows read transactions to happen while an update is in progress,
    because update transactions don't mutate the store's internal data
    structures until commit time.
    
    Currently, there is a limit of one concurrent update transaction, to
    make sure that updates don't conflict with each other, but this could be
    changed.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- state/memory_test.go
+++ state/memory_test.go
@@ -99,7 +99,7 @@ func TestStoreNode(t *testing.T) {
 		assert.Len(t, foundNodes, 1)
 
 		invalidUpdate := *nodeSet[0]
-		invalidUpdate.Spec.ID = "invalid"
+		invalidUpdate.Spec = &api.NodeSpec{ID: "invalid"}
 		assert.Error(t, tx.Nodes().Update(&invalidUpdate), "invalid IDs should be rejected")
 
 		// Delete
@@ -481,161 +481,206 @@ func TestStoreSnapshot(t *testing.T) {
 	defer s1.WatchQueue().StopWatch(watcher)
 	assert.NoError(t, err)
 
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, nodeSet[0], tx2.Nodes().Get("id1"))
+		assert.Equal(t, nodeSet[1], tx2.Nodes().Get("id2"))
+		assert.Equal(t, nodeSet[2], tx2.Nodes().Get("id3"))
+
+		assert.Equal(t, jobSet[0], tx2.Jobs().Get("id1"))
+		assert.Equal(t, jobSet[1], tx2.Jobs().Get("id2"))
+		assert.Equal(t, jobSet[2], tx2.Jobs().Get("id3"))
+
+		assert.Equal(t, taskSet[0], tx2.Tasks().Get("id1"))
+		assert.Equal(t, taskSet[1], tx2.Tasks().Get("id2"))
+		assert.Equal(t, taskSet[2], tx2.Tasks().Get("id3"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Create node
+	createNode := &api.Node{
+		Spec: &api.NodeSpec{
+			ID: "id4",
+			Meta: &api.Meta{
+				Name: "name4",
+			},
+		},
+	}
+
 	err = s1.Update(func(tx1 Tx) error {
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, nodeSet[0], tx2.Nodes().Get("id1"))
-			assert.Equal(t, nodeSet[1], tx2.Nodes().Get("id2"))
-			assert.Equal(t, nodeSet[2], tx2.Nodes().Get("id3"))
-
-			assert.Equal(t, jobSet[0], tx2.Jobs().Get("id1"))
-			assert.Equal(t, jobSet[1], tx2.Jobs().Get("id2"))
-			assert.Equal(t, jobSet[2], tx2.Jobs().Get("id3"))
-
-			assert.Equal(t, taskSet[0], tx2.Tasks().Get("id1"))
-			assert.Equal(t, taskSet[1], tx2.Tasks().Get("id2"))
-			assert.Equal(t, taskSet[2], tx2.Tasks().Get("id3"))
-			return nil
-		})
-		assert.NoError(t, err)
-
-		// Create node
-		createNode := &api.Node{
-			Spec: &api.NodeSpec{
-				ID: "id4",
-				Meta: &api.Meta{
-					Name: "name4",
-				},
-			},
-		}
 		assert.NoError(t, tx1.Nodes().Create(createNode))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, createNode, tx2.Nodes().Get("id4"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		// Update node
-		updateNode := &api.Node{
-			Spec: &api.NodeSpec{
-				ID: "id3",
-				Meta: &api.Meta{
-					Name: "name3",
-				},
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, createNode, tx2.Nodes().Get("id4"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Update node
+	updateNode := &api.Node{
+		Spec: &api.NodeSpec{
+			ID: "id3",
+			Meta: &api.Meta{
+				Name: "name3",
 			},
-		}
+		},
+	}
+
+	err = s1.Update(func(tx1 Tx) error {
 		assert.NoError(t, tx1.Nodes().Update(updateNode))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, updateNode, tx2.Nodes().Get("id3"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, updateNode, tx2.Nodes().Get("id3"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	err = s1.Update(func(tx1 Tx) error {
 		// Delete node
 		assert.NoError(t, tx1.Nodes().Delete("id1"))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Nil(t, tx2.Nodes().Get("id1"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		// Create job
-		createJob := &api.Job{
-			ID: "id4",
-			Spec: &api.JobSpec{
-				Meta: &api.Meta{
-					Name: "name4",
-				},
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Nil(t, tx2.Nodes().Get("id1"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Create job
+	createJob := &api.Job{
+		ID: "id4",
+		Spec: &api.JobSpec{
+			Meta: &api.Meta{
+				Name: "name4",
 			},
-		}
+		},
+	}
+
+	err = s1.Update(func(tx1 Tx) error {
 		assert.NoError(t, tx1.Jobs().Create(createJob))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, createJob, tx2.Jobs().Get("id4"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		// Update job
-		updateJob := &api.Job{
-			ID: "id3",
-			Spec: &api.JobSpec{
-				Meta: &api.Meta{
-					Name: "name3",
-				},
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, createJob, tx2.Jobs().Get("id4"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Update job
+	updateJob := &api.Job{
+		ID: "id3",
+		Spec: &api.JobSpec{
+			Meta: &api.Meta{
+				Name: "name3",
 			},
-		}
+		},
+	}
+
+	err = s1.Update(func(tx1 Tx) error {
 		assert.NotEqual(t, updateJob, tx1.Jobs().Get("id3"))
 		assert.NoError(t, tx1.Jobs().Update(updateJob))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, updateJob, tx2.Jobs().Get("id3"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, updateJob, tx2.Jobs().Get("id3"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	err = s1.Update(func(tx1 Tx) error {
 		// Delete job
 		assert.NoError(t, tx1.Jobs().Delete("id1"))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Nil(t, tx1.Jobs().Get("id1"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		// Create task
-		createTask := &api.Task{
-			ID: "id4",
-			Spec: &api.JobSpec{
-				Meta: &api.Meta{
-					Name: "name4",
-				},
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Nil(t, tx2.Jobs().Get("id1"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Create task
+	createTask := &api.Task{
+		ID: "id4",
+		Spec: &api.JobSpec{
+			Meta: &api.Meta{
+				Name: "name4",
 			},
-		}
+		},
+	}
+
+	err = s1.Update(func(tx1 Tx) error {
 		assert.NoError(t, tx1.Tasks().Create(createTask))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, createTask, tx2.Tasks().Get("id4"))
-			return nil
-		})
-		assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		// Update task
-		updateTask := &api.Task{
-			ID: "id3",
-			Spec: &api.JobSpec{
-				Meta: &api.Meta{
-					Name: "name3",
-				},
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, createTask, tx2.Tasks().Get("id4"))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Update task
+	updateTask := &api.Task{
+		ID: "id3",
+		Spec: &api.JobSpec{
+			Meta: &api.Meta{
+				Name: "name3",
 			},
-		}
+		},
+	}
+
+	err = s1.Update(func(tx1 Tx) error {
 		assert.NoError(t, tx1.Tasks().Update(updateTask))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Equal(t, updateTask, tx2.Tasks().Get("id3"))
-			return nil
-		})
-		assert.NoError(t, err)
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Equal(t, updateTask, tx2.Tasks().Get("id3"))
+		return nil
+	})
+	assert.NoError(t, err)
 
+	err = s1.Update(func(tx1 Tx) error {
 		// Delete task
 		assert.NoError(t, tx1.Tasks().Delete("id1"))
-		assert.NoError(t, Apply(s2, <-watcher))
+		return nil
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, Apply(s2, <-watcher))
 
-		err = s2.View(func(tx2 ReadTx) error {
-			assert.Nil(t, tx2.Tasks().Get("id1"))
-			return nil
-		})
-		assert.NoError(t, err)
+	err = s2.View(func(tx2 ReadTx) error {
+		assert.Nil(t, tx2.Tasks().Get("id1"))
 		return nil
 	})
 	assert.NoError(t, err)
