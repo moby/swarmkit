bb7f8051b3993e0bf2bb74475b311a6e3a9ac9b2
Victor Vieux <vieux@docker.com>
2016-03-03 17:01:45 -0800

    add validations and test
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- manager/clusterapi/job.go
+++ manager/clusterapi/job.go
@@ -9,15 +9,78 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
+// TODO(vieux): refactor validation once we have more objects to validate
+func validateJobSpecMeta(m *api.Meta) error {
+	if m == nil {
+		return grpc.Errorf(codes.InvalidArgument, "meta: required in job spec")
+	}
+	if m.Name == "" {
+		return grpc.Errorf(codes.InvalidArgument, "meta: name must be provided")
+	}
+	return nil
+}
+
+func validateJobSpecSource(s *api.Source) error {
+	if s == nil {
+		return grpc.Errorf(codes.InvalidArgument, "source: required in job spec")
+	}
+	image := s.GetImage()
+	if image == nil {
+		return grpc.Errorf(codes.Unimplemented, "source: invalid source type. only image is supposed")
+	}
+	if image.Reference == "" {
+		return grpc.Errorf(codes.InvalidArgument, "source: reference must be provided")
+	}
+	return nil
+}
+
+func validateJobSpecOrchestration(o *api.JobSpec_Orchestration) error {
+	if o == nil {
+		return grpc.Errorf(codes.InvalidArgument, "orchestration: required in job spec")
+	}
+	job := o.GetJob()
+	if job == nil {
+		return grpc.Errorf(codes.InvalidArgument, "orchestration: job must be provided")
+	}
+	switch job.(type) {
+	case *api.JobSpec_Orchestration_Batch:
+		return grpc.Errorf(codes.Unimplemented, "orchestration: batch is not supported")
+	case *api.JobSpec_Orchestration_Cron:
+		return grpc.Errorf(codes.Unimplemented, "orchestration: cron is not supported")
+	case *api.JobSpec_Orchestration_Global:
+		return grpc.Errorf(codes.Unimplemented, "orchestration: global is not supported")
+	case *api.JobSpec_Orchestration_Service:
+		if o.GetService() == nil {
+			return grpc.Errorf(codes.InvalidArgument, "orchestration: service must be provided")
+		}
+	}
+	return nil
+}
+
+func validateJobSpec(spec *api.JobSpec) error {
+	if spec == nil {
+		return grpc.Errorf(codes.InvalidArgument, errInvalidArgument.Error())
+	}
+	if err := validateJobSpecMeta(spec.Meta); err != nil {
+		return err
+	}
+	if err := validateJobSpecSource(spec.Source); err != nil {
+		return err
+	}
+	if err := validateJobSpecOrchestration(spec.Orchestration); err != nil {
+		return err
+	}
+	return nil
+}
+
 // CreateJob creates and return a Job based on the provided JobSpec.
 // - Returns `InvalidArgument` if the JobSpec is malformed.
 // - Returns `Unimplemented` if the JobSpec references unimplemented features.
 // - Returns `AlreadyExists` if the JobID conflicts.
 // - Returns an error if the creation fails.
 func (s *Server) CreateJob(ctx context.Context, request *api.CreateJobRequest) (*api.CreateJobResponse, error) {
-	// TODO(aluzzardi): We need to validate that JobSpec.
-	if request.Spec == nil {
-		return nil, grpc.Errorf(codes.InvalidArgument, errInvalidArgument.Error())
+	if err := validateJobSpec(request.Spec); err != nil {
+		return nil, err
 	}
 
 	// TODO(aluzzardi): Consider using `Name` as a primary key to handle
--- manager/clusterapi/job_test.go
+++ manager/clusterapi/job_test.go
@@ -10,13 +10,141 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
+func createSpec(name, image string, instances int64) *api.JobSpec {
+	return &api.JobSpec{
+		Meta: &api.Meta{
+			Name: name,
+		},
+		Source: &api.Source{
+			Source: &api.Source_Image{
+				Image: &api.ImageSpec{
+					Reference: image,
+				},
+			},
+		},
+		Orchestration: &api.JobSpec_Orchestration{
+			Job: &api.JobSpec_Orchestration_Service{
+				Service: &api.JobSpec_ServiceJob{
+					Instances: instances,
+				},
+			},
+		},
+	}
+}
+
+func createJob(ts *testServer, name, image string, instances int64) *api.Job {
+	spec := createSpec(name, image, instances)
+	r, _ := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: spec})
+	return r.Job
+}
+
+func TestValidateJobSpecMeta(t *testing.T) {
+	var m *api.Meta
+	err := validateJobSpecMeta(m)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	m = &api.Meta{}
+	err = validateJobSpecMeta(m)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	m.Name = "name"
+	err = validateJobSpecMeta(m)
+	assert.NoError(t, err)
+}
+
+func TestValidateJobSpecSource(t *testing.T) {
+	var s *api.Source
+	err := validateJobSpecSource(s)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	s = &api.Source{}
+	err = validateJobSpecSource(s)
+	assert.Error(t, err)
+	assert.Equal(t, codes.Unimplemented, grpc.Code(err))
+
+	s.Source = &api.Source_Image{
+		Image: &api.ImageSpec{},
+	}
+
+	err = validateJobSpecSource(s)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	s.GetImage().Reference = "image"
+	err = validateJobSpecSource(s)
+	assert.NoError(t, err)
+}
+
+func TestValidateJobSpecOrchestration(t *testing.T) {
+	var o *api.JobSpec_Orchestration
+	err := validateJobSpecOrchestration(o)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	o = &api.JobSpec_Orchestration{}
+	err = validateJobSpecOrchestration(o)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	o.Job = &api.JobSpec_Orchestration_Batch{}
+	err = validateJobSpecOrchestration(o)
+	assert.Error(t, err)
+	assert.Equal(t, codes.Unimplemented, grpc.Code(err))
+
+	o.Job = &api.JobSpec_Orchestration_Service{}
+	err = validateJobSpecOrchestration(o)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	o.Job = &api.JobSpec_Orchestration_Service{
+		Service: &api.JobSpec_ServiceJob{
+			Instances: 1,
+		},
+	}
+	err = validateJobSpecOrchestration(o)
+	assert.NoError(t, err)
+}
+
+func TestValidateJobSpec(t *testing.T) {
+	var spec *api.JobSpec
+	err := validateJobSpec(spec)
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	spec = &api.JobSpec{
+		Meta: &api.Meta{
+			Name: "name",
+		},
+		Source: &api.Source{
+			Source: &api.Source_Image{
+				Image: &api.ImageSpec{
+					Reference: "image",
+				},
+			},
+		},
+		Orchestration: &api.JobSpec_Orchestration{
+			Job: &api.JobSpec_Orchestration_Service{
+				Service: &api.JobSpec_ServiceJob{
+					Instances: 1,
+				},
+			},
+		},
+	}
+	err = validateJobSpec(spec)
+	assert.NoError(t, err)
+}
+
 func TestCreateJob(t *testing.T) {
 	ts := newTestServer(t)
 	_, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{})
 	assert.Error(t, err)
 	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
 
-	r, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	spec := createSpec("name", "image", 1)
+	r, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: spec})
 	assert.NoError(t, err)
 	assert.NotEmpty(t, r.Job.ID)
 }
@@ -27,11 +155,10 @@ func TestGetJob(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
 
-	rc, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	job := createJob(ts, "name", "image", 1)
+	r, err := ts.Client.GetJob(context.Background(), &api.GetJobRequest{JobID: job.ID})
 	assert.NoError(t, err)
-	rg, err := ts.Client.GetJob(context.Background(), &api.GetJobRequest{JobID: rc.Job.ID})
-	assert.NoError(t, err)
-	assert.Equal(t, rc.Job, rg.Job)
+	assert.Equal(t, job, r.Job)
 }
 
 func TestUpdateJob(t *testing.T) {
@@ -47,11 +174,10 @@ func TestDeleteJob(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
 
-	rc, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	job := createJob(ts, "name", "image", 1)
+	r, err := ts.Client.DeleteJob(context.Background(), &api.DeleteJobRequest{JobID: job.ID})
 	assert.NoError(t, err)
-	rd, err := ts.Client.DeleteJob(context.Background(), &api.DeleteJobRequest{JobID: rc.Job.ID})
-	assert.NoError(t, err)
-	assert.NotNil(t, rd)
+	assert.NotNil(t, r)
 }
 
 func TestListJobs(t *testing.T) {
@@ -60,9 +186,14 @@ func TestListJobs(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, len(r.Jobs))
 
-	_, err = ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
-	assert.NoError(t, err)
+	_ = createJob(ts, "name1", "image", 1)
 	r, err = ts.Client.ListJobs(context.Background(), &api.ListJobsRequest{})
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(r.Jobs))
+
+	_ = createJob(ts, "name2", "image", 1)
+	_ = createJob(ts, "name3", "image", 1)
+	r, err = ts.Client.ListJobs(context.Background(), &api.ListJobsRequest{})
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(r.Jobs))
 }
