1a8ebc3d4ee2da8b5159fb94bef460916714b6bc
Andrea Luzzardi <aluzzardi@gmail.com>
2016-03-03 16:06:29 -0800

    swarmctl: Dummy `task ls` implementation to try out scheduling
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cmd/swarmctl/task/ls.go
+++ cmd/swarmctl/task/ls.go
@@ -1,12 +1,38 @@
 package task
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/docker/swarm-v2/api"
+	"github.com/docker/swarm-v2/cmd/swarmctl/common"
+	"github.com/spf13/cobra"
+)
 
 var (
 	lsCmd = &cobra.Command{
 		Use:   "ls",
 		Short: "List tasks",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			c, err := common.Dial(cmd)
+			if err != nil {
+				return err
+			}
+			r, err := c.ListTasks(common.Context(cmd), &api.ListTasksRequest{})
+			if err != nil {
+				return err
+			}
+
+			w := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
+			defer func() {
+				// Ignore flushing errors - there's nothing we can do.
+				_ = w.Flush()
+			}()
+			fmt.Fprintln(w, "ID\tJobID\tNodeID")
+			for _, t := range r.Tasks {
+				fmt.Fprintf(w, "%s\t%s\t%s\n", t.ID, t.JobID, t.NodeID)
+			}
 			return nil
 		},
 	}
