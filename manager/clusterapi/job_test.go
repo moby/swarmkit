55b917a0bb1ebcada048ecdbbc0160278d7c9201
Victor Vieux <vieux@docker.com>
2016-03-03 16:13:41 -0800

    add some tests in clusterapi create
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- manager/clusterapi/job_test.go
+++ manager/clusterapi/job_test.go
@@ -11,11 +11,27 @@ import (
 )
 
 func TestCreateJob(t *testing.T) {
-	// TODO
+	ts := newTestServer(t)
+	_, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{})
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	r, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, r.Job.ID)
 }
 
 func TestGetJob(t *testing.T) {
-	// TODO
+	ts := newTestServer(t)
+	_, err := ts.Client.GetJob(context.Background(), &api.GetJobRequest{})
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	rc, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	assert.NoError(t, err)
+	rg, err := ts.Client.GetJob(context.Background(), &api.GetJobRequest{JobID: rc.Job.ID})
+	assert.NoError(t, err)
+	assert.Equal(t, rc.Job, rg.Job)
 }
 
 func TestUpdateJob(t *testing.T) {
@@ -26,9 +42,27 @@ func TestUpdateJob(t *testing.T) {
 }
 
 func TestDeleteJob(t *testing.T) {
-	// TODO
+	ts := newTestServer(t)
+	_, err := ts.Client.DeleteJob(context.Background(), &api.DeleteJobRequest{})
+	assert.Error(t, err)
+	assert.Equal(t, codes.InvalidArgument, grpc.Code(err))
+
+	rc, err := ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	assert.NoError(t, err)
+	rd, err := ts.Client.DeleteJob(context.Background(), &api.DeleteJobRequest{JobID: rc.Job.ID})
+	assert.NoError(t, err)
+	assert.NotNil(t, rd)
 }
 
 func TestListJobs(t *testing.T) {
-	// TODO
+	ts := newTestServer(t)
+	r, err := ts.Client.ListJobs(context.Background(), &api.ListJobsRequest{})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(r.Jobs))
+
+	_, err = ts.Client.CreateJob(context.Background(), &api.CreateJobRequest{Spec: &api.JobSpec{}})
+	assert.NoError(t, err)
+	r, err = ts.Client.ListJobs(context.Background(), &api.ListJobsRequest{})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(r.Jobs))
 }
