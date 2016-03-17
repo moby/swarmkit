7657c443c09d8235b70a52d8dd281c8fdb3dc161
Stephen J Day <stephen.day@docker.com>
2016-03-16 20:49:19 -0700

    cmd/agent: allow one to specify address of engine
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- agent/config.go
+++ agent/config.go
@@ -1,6 +1,10 @@
 package agent
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/docker/swarm-v2/agent/exec"
+)
 
 // Config provides values for an Agent.
 type Config struct {
@@ -13,6 +17,9 @@ type Config struct {
 	// Managers provides the manager backend used by the agent. It will be
 	// updated with managers weights as observed by the agent.
 	Managers Managers
+
+	// Executor specifies the executor to use for the agent.
+	Executor exec.Executor
 }
 
 func (c *Config) validate() error {
