e6ceae078d97222606333e9c936370d2ceb0d121
Victor Vieux <vieux@docker.com>
2016-03-17 03:40:27 -0700

    support re-deploy
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- cmd/swarmctl/root/deploy.go
+++ cmd/swarmctl/root/deploy.go
@@ -2,6 +2,7 @@ package root
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/docker/swarm-v2/api"
 	"github.com/docker/swarm-v2/cmd/swarmctl/common"
@@ -22,12 +23,36 @@ var (
 			if err != nil {
 				return err
 			}
-			for _, jobSpec := range s.JobSpecs() {
-				r, err := c.CreateJob(common.Context(cmd), &api.CreateJobRequest{Spec: jobSpec})
-				if err != nil {
-					return err
+
+			r, err := c.ListJobs(common.Context(cmd), &api.ListJobsRequest{})
+			if err != nil {
+				return err
+			}
+
+			jobs := map[string]*api.Job{}
+
+			for _, j := range r.Jobs {
+				if j.Spec.Meta.Labels["namespace"] == s.Namespace {
+					jobs[j.Spec.Meta.Name] = j
 				}
-				fmt.Printf("%s: %s\n", jobSpec.Meta.Name, r.Job.ID)
+			}
+
+			for _, jobSpec := range s.JobSpecs() {
+				if job, ok := jobs[jobSpec.Meta.Name]; ok && !reflect.DeepEqual(job.Spec, jobSpec) {
+					r, err := c.UpdateJob(common.Context(cmd), &api.UpdateJobRequest{JobID: job.ID, Spec: jobSpec})
+					if err != nil {
+						fmt.Printf("%s: %v", jobSpec.Meta.Name, err)
+						continue
+					}
+					fmt.Printf("%s: %s\n", jobSpec.Meta.Name, r.Job.ID)
+				} else if !ok {
+					r, err := c.CreateJob(common.Context(cmd), &api.CreateJobRequest{Spec: jobSpec})
+					if err != nil {
+						fmt.Printf("%s: %v", jobSpec.Meta.Name, err)
+						continue
+					}
+					fmt.Printf("%s: %s\n", jobSpec.Meta.Name, r.Job.ID)
+				}
 			}
 			return nil
 		},
