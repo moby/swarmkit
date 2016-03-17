73dac9437a83e53339c7df715e24675b32862f0c
Victor Vieux <vieux@docker.com>
2016-03-17 10:06:14 -0700

    add -q on list commands
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- cmd/swarmctl/job/ls.go
+++ cmd/swarmctl/job/ls.go
@@ -16,6 +16,13 @@ var (
 		Use:   "ls",
 		Short: "List jobs",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			flags := cmd.Flags()
+
+			quiet, err := flags.GetBool("quiet")
+			if err != nil {
+				return err
+			}
+
 			c, err := common.Dial(cmd)
 			if err != nil {
 				return err
@@ -25,32 +32,47 @@ var (
 				return err
 			}
 
-			w := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
-			defer func() {
-				// Ignore flushing errors - there's nothing we can do.
-				_ = w.Flush()
-			}()
-			fmt.Fprintln(w, "ID\tName\tImage\tInstances")
-			for _, j := range r.Jobs {
-				spec := j.Spec
-				service := spec.GetService()
-				image := spec.Template.GetContainer().Image
+			var output func(j *api.Job)
 
-				// TODO(aluzzardi): Right now we only implement the happy path
-				// and don't have any proper error handling whatsover.
-				// Instead of aborting, we should display what we can of the job.
-				if service == nil || image == nil {
-					log.Fatalf("Malformed job: %v", j)
+			if !quiet {
+				w := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
+				defer func() {
+					// Ignore flushing errors - there's nothing we can do.
+					_ = w.Flush()
+				}()
+				fmt.Fprintln(w, "ID\tName\tImage\tInstances")
+				output = func(j *api.Job) {
+					spec := j.Spec
+					service := spec.GetService()
+					image := spec.Template.GetContainer().Image
+
+					// TODO(aluzzardi): Right now we only implement the happy path
+					// and don't have any proper error handling whatsover.
+					// Instead of aborting, we should display what we can of the job.
+					if service == nil || image == nil {
+						log.Fatalf("Malformed job: %v", j)
+					}
+
+					fmt.Fprintf(w, "%s\t%s\t%s\t%d\n",
+						j.ID,
+						spec.Meta.Name,
+						image.Reference,
+						service.Instances,
+					)
 				}
 
-				fmt.Fprintf(w, "%s\t%s\t%s\t%d\n",
-					j.ID,
-					spec.Meta.Name,
-					image.Reference,
-					service.Instances,
-				)
+			} else {
+				output = func(j *api.Job) { fmt.Println(j.ID) }
+			}
+
+			for _, j := range r.Jobs {
+				output(j)
 			}
 			return nil
 		},
 	}
 )
+
+func init() {
+	lsCmd.Flags().BoolP("quiet", "q", false, "Only display IDs")
+}
