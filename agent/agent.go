480c56fae66607174e24dbd57290c86dd1c4f813
Stephen J Day <stephen.day@docker.com>
2016-04-21 12:35:36 -0700

    agent: more consistent style for logs during state transition
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- agent/agent.go
+++ agent/agent.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 	"reflect"
 	"time"
@@ -379,8 +380,6 @@ func (a *Agent) handleTaskStatusReport(ctx context.Context, session *session, re
 }
 
 func (a *Agent) updateStatus(ctx context.Context, report taskStatusReport) error {
-	ctx = log.WithLogger(ctx, log.G(ctx).WithField("task.id", report.taskID))
-
 	status, ok := a.statuses[report.taskID]
 	if !ok {
 		return errTaskUnknown
@@ -391,10 +390,14 @@ func (a *Agent) updateStatus(ctx context.Context, report taskStatusReport) error
 	}
 
 	original := status.Copy()
+	ctx = log.WithLogger(ctx, log.G(ctx).WithFields(
+		logrus.Fields{
+			"state.transition": fmt.Sprintf("%v->%v", original.State, report.state),
+			"task.id":          report.taskID}))
 
 	// validate transition only moves forward or updates fields
 	if report.state < status.State && report.err == nil {
-		log.G(ctx).Errorf("%v -> %v invalid!", status.State, report.state)
+		log.G(ctx).Error("invalid transition")
 		return errTaskInvalidStateTransition
 	}
 
@@ -420,7 +423,7 @@ func (a *Agent) updateStatus(ctx context.Context, report taskStatusReport) error
 		case api.TaskStateFinalize:
 			if task.DesiredState >= api.TaskStateDead {
 				if err := a.removeTask(ctx, task.Copy()); err != nil {
-					log.G(ctx).WithError(err).Error("failed retrying remove task")
+					log.G(ctx).WithError(err).Error("failed removing task")
 				}
 			}
 		}
@@ -445,8 +448,6 @@ func (a *Agent) updateStatus(ctx context.Context, report taskStatusReport) error
 	}
 
 	log.G(ctx).WithFields(logrus.Fields{
-		"state.from":    original.State,
-		"state.to":      status.State,
 		"state.message": status.Message,
 	}).Infof("task status updated")
 
