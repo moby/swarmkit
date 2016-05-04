6eda6436348a40a806f05b479d1891013b5f6792
Aaron Lehmann <aaron.lehmann@docker.com>
2016-05-04 12:54:18 -0700

    Add limit on number of restarts
    Add MaxAttempts and Window fields to RestartPolicy.  MaxAttempts is the
    number of restart attempts before giving up, if nonzero. Window is the
    period to look back when enforcing the restart policy. If it is zero, the
    lookback is indefinite.
    
    Enforce these parameters in RestartSupervisor, using local state. Add
    unit tests.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- manager/orchestrator/restart.go
+++ manager/orchestrator/restart.go
@@ -1,6 +1,7 @@
 package orchestrator
 
 import (
+	"container/list"
 	"sync"
 	"time"
 
@@ -10,20 +11,37 @@ import (
 	"golang.org/x/net/context"
 )
 
+type restartedInstance struct {
+	timestamp time.Time
+}
+
+type instanceRestartInfo struct {
+	// counter of restarts for this instance.
+	totalRestarts uint64
+	// Linked list of restartedInstance structs. Only used when
+	// Restart.MaxAttempts and Restart.Window are both
+	// nonzero.
+	restartedInstances *list.List
+}
+
 // RestartSupervisor initiates and manages restarts. It's responsible for
 // delaying restarts when applicable.
 type RestartSupervisor struct {
-	mu       sync.Mutex
-	store    *store.MemoryStore
-	delays   map[string]*time.Timer
-	delaysWG sync.WaitGroup
+	mu               sync.Mutex
+	store            *store.MemoryStore
+	delays           map[string]*time.Timer
+	timersWG         sync.WaitGroup
+	history          map[instanceTuple]*instanceRestartInfo
+	historyByService map[string]map[instanceTuple]struct{}
 }
 
 // NewRestartSupervisor creates a new RestartSupervisor.
 func NewRestartSupervisor(store *store.MemoryStore) *RestartSupervisor {
 	return &RestartSupervisor{
-		store:  store,
-		delays: make(map[string]*time.Timer),
+		store:            store,
+		delays:           make(map[string]*time.Timer),
+		history:          make(map[instanceTuple]*instanceRestartInfo),
+		historyByService: make(map[string]map[instanceTuple]struct{}),
 	}
 }
 
@@ -37,10 +55,7 @@ func (r *RestartSupervisor) Restart(ctx context.Context, tx store.Tx, service *a
 		return err
 	}
 
-	condition := restartCondition(service)
-
-	if condition != api.RestartAlways &&
-		(condition != api.RestartOnFailure || t.Status.TerminalState == api.TaskStateCompleted) {
+	if !r.shouldRestart(&t, service) {
 		return nil
 	}
 
@@ -64,16 +79,119 @@ func (r *RestartSupervisor) Restart(ctx context.Context, tx store.Tx, service *a
 		log.G(ctx).WithError(err).WithField("task.id", restartTask.ID).Error("task create failed")
 		return err
 	}
+
+	r.recordRestartHistory(restartTask, service)
+
 	if restartTask.DesiredState == api.TaskStateReady {
 		r.DelayStart(ctx, restartTask.ID, service.Spec.Restart.Delay)
 	}
 	return nil
 }
 
+func (r *RestartSupervisor) shouldRestart(t *api.Task, service *api.Service) bool {
+	condition := restartCondition(service)
+
+	if condition != api.RestartAlways &&
+		(condition != api.RestartOnFailure || t.Status.TerminalState == api.TaskStateCompleted) {
+		return false
+	}
+
+	if service.Spec.Restart == nil || service.Spec.Restart.MaxAttempts == 0 {
+		return true
+	}
+
+	instanceTuple := instanceTuple{
+		instance:  t.Instance,
+		serviceID: t.ServiceID,
+	}
+
+	// Instance is not meaningful for "fill" tasks, so they need to be
+	// indexed by NodeID.
+	if service.Spec.Mode == api.ServiceModeFill {
+		instanceTuple.nodeID = t.NodeID
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	restartInfo := r.history[instanceTuple]
+	if restartInfo == nil {
+		return true
+	}
+
+	if service.Spec.Restart.Window == 0 {
+		return restartInfo.totalRestarts < service.Spec.Restart.MaxAttempts
+	}
+
+	if restartInfo.restartedInstances == nil {
+		return true
+	}
+
+	lookback := time.Now().Add(-service.Spec.Restart.Window)
+
+	var next *list.Element
+	for e := restartInfo.restartedInstances.Front(); e != nil; e = next {
+		next = e.Next()
+
+		if e.Value.(restartedInstance).timestamp.After(lookback) {
+			break
+		}
+		restartInfo.restartedInstances.Remove(e)
+	}
+
+	numRestarts := uint64(restartInfo.restartedInstances.Len())
+
+	if numRestarts == 0 {
+		restartInfo.restartedInstances = nil
+	}
+
+	return numRestarts < service.Spec.Restart.MaxAttempts
+}
+
+func (r *RestartSupervisor) recordRestartHistory(restartTask *api.Task, service *api.Service) {
+	if service.Spec.Restart == nil || service.Spec.Restart.MaxAttempts == 0 {
+		// No limit on the number of restarts, so no need to record
+		// history.
+		return
+	}
+	tuple := instanceTuple{
+		instance:  restartTask.Instance,
+		serviceID: restartTask.ServiceID,
+		nodeID:    restartTask.NodeID,
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if r.history[tuple] == nil {
+		r.history[tuple] = &instanceRestartInfo{}
+	}
+	restartInfo := r.history[tuple]
+	restartInfo.totalRestarts++
+
+	if r.historyByService[restartTask.ServiceID] == nil {
+		r.historyByService[restartTask.ServiceID] = make(map[instanceTuple]struct{})
+	}
+	r.historyByService[restartTask.ServiceID][tuple] = struct{}{}
+
+	if service.Spec.Restart.Window != 0 {
+		if restartInfo.restartedInstances == nil {
+			restartInfo.restartedInstances = list.New()
+		}
+
+		restartedInstance := restartedInstance{
+			timestamp: time.Now(),
+		}
+
+		restartInfo.restartedInstances.PushBack(restartedInstance)
+	}
+}
+
 // DelayStart starts a timer that starts the task after a specified delay.
 func (r *RestartSupervisor) DelayStart(ctx context.Context, taskID string, delay time.Duration) {
 	r.mu.Lock()
-	r.delaysWG.Add(1)
+	r.timersWG.Add(1)
+
 	timer := time.AfterFunc(delay,
 		func() {
 			err := r.store.Update(func(tx store.Tx) error {
@@ -89,14 +207,14 @@ func (r *RestartSupervisor) DelayStart(ctx context.Context, taskID string, delay
 			r.mu.Lock()
 			delete(r.delays, taskID)
 			r.mu.Unlock()
-			r.delaysWG.Done()
+			r.timersWG.Done()
 		})
 
 	if oldTimer, ok := r.delays[taskID]; ok {
 		// Task ID collision should be extremely rare
 		log.G(ctx).WithField("task.id", taskID).Error("start delay timer already exists for this task ID - replacing")
 		if oldTimer.Stop() {
-			r.delaysWG.Done()
+			r.timersWG.Done()
 		}
 	}
 	r.delays[taskID] = timer
@@ -118,7 +236,7 @@ func (r *RestartSupervisor) StartNow(tx store.Tx, taskID string) error {
 func (r *RestartSupervisor) Cancel(taskID string) {
 	r.mu.Lock()
 	if timer, ok := r.delays[taskID]; ok && timer.Stop() {
-		r.delaysWG.Done()
+		r.timersWG.Done()
 		delete(r.delays, taskID)
 	}
 	r.mu.Unlock()
@@ -130,10 +248,28 @@ func (r *RestartSupervisor) CancelAll() {
 	r.mu.Lock()
 	for _, timer := range r.delays {
 		if timer.Stop() {
-			r.delaysWG.Done()
+			r.timersWG.Done()
 		}
 	}
 	r.delays = make(map[string]*time.Timer)
+	r.history = make(map[instanceTuple]*instanceRestartInfo)
 	r.mu.Unlock()
-	r.delaysWG.Wait()
+	r.timersWG.Wait()
+}
+
+// ClearServiceHistory forgets restart history related to a given service ID.
+func (r *RestartSupervisor) ClearServiceHistory(serviceID string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	tuples := r.historyByService[serviceID]
+	if tuples == nil {
+		return
+	}
+
+	delete(r.historyByService, serviceID)
+
+	for t := range tuples {
+		delete(r.history, t)
+	}
 }
--- manager/orchestrator/restart_test.go
+++ manager/orchestrator/restart_test.go
@@ -17,6 +17,7 @@ func TestOrchestratorRestartAlways(t *testing.T) {
 	assert.NotNil(t, s)
 
 	orchestrator := New(s)
+	defer orchestrator.Stop()
 
 	watch, cancel := state.Watch(s.WatchQueue() /*state.EventCreateTask{}, state.EventUpdateTask{}*/)
 	defer cancel()
@@ -90,8 +91,6 @@ func TestOrchestratorRestartAlways(t *testing.T) {
 	observedTask4 := watchTaskCreate(t, watch)
 	assert.Equal(t, observedTask4.Status.State, api.TaskStateNew)
 	assert.Equal(t, observedTask4.Annotations.Name, "name1")
-
-	orchestrator.Stop()
 }
 
 func TestOrchestratorRestartOnFailure(t *testing.T) {
@@ -100,6 +99,7 @@ func TestOrchestratorRestartOnFailure(t *testing.T) {
 	assert.NotNil(t, s)
 
 	orchestrator := New(s)
+	defer orchestrator.Stop()
 
 	watch, cancel := state.Watch(s.WatchQueue() /*state.EventCreateTask{}, state.EventUpdateTask{}*/)
 	defer cancel()
@@ -177,8 +177,6 @@ func TestOrchestratorRestartOnFailure(t *testing.T) {
 		t.Fatal("got unexpected event")
 	case <-time.After(100 * time.Millisecond):
 	}
-
-	orchestrator.Stop()
 }
 
 func TestOrchestratorRestartNever(t *testing.T) {
@@ -187,6 +185,7 @@ func TestOrchestratorRestartNever(t *testing.T) {
 	assert.NotNil(t, s)
 
 	orchestrator := New(s)
+	defer orchestrator.Stop()
 
 	watch, cancel := state.Watch(s.WatchQueue() /*state.EventCreateTask{}, state.EventUpdateTask{}*/)
 	defer cancel()
@@ -266,8 +265,6 @@ func TestOrchestratorRestartNever(t *testing.T) {
 		t.Fatal("got unexpected event")
 	case <-time.After(100 * time.Millisecond):
 	}
-
-	orchestrator.Stop()
 }
 
 func TestOrchestratorRestartDelay(t *testing.T) {
@@ -276,6 +273,7 @@ func TestOrchestratorRestartDelay(t *testing.T) {
 	assert.NotNil(t, s)
 
 	orchestrator := New(s)
+	defer orchestrator.Stop()
 
 	watch, cancel := state.Watch(s.WatchQueue() /*state.EventCreateTask{}, state.EventUpdateTask{}*/)
 	defer cancel()
@@ -337,19 +335,89 @@ func TestOrchestratorRestartDelay(t *testing.T) {
 	assert.Equal(t, observedTask3.DesiredState, api.TaskStateReady)
 	assert.Equal(t, observedTask3.Annotations.Name, "name1")
 
-	var observedTask4 *api.Task
+	observedTask4 := watchTaskUpdate(t, watch)
+	after := time.Now()
 
-	select {
-	case e := <-watch:
-		if updateEvent, ok := e.(state.EventUpdateTask); ok {
-			observedTask4 = updateEvent.Task
-		} else {
-			t.Fatal("got unexpected event")
-		}
-	case <-time.After(time.Second):
-		t.Fatal("expected TaskUpdate event")
+	// At least 100 ms should have elapsed. Only check the lower bound,
+	// because the system may be slow and it could have taken longer.
+	if after.Sub(before) < 100*time.Millisecond {
+		t.Fatal("restart delay should have elapsed")
 	}
 
+	assert.Equal(t, observedTask4.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask4.DesiredState, api.TaskStateRunning)
+	assert.Equal(t, observedTask4.Annotations.Name, "name1")
+}
+
+func TestOrchestratorRestartMaxAttempts(t *testing.T) {
+	ctx := context.Background()
+	s := store.NewMemoryStore(nil)
+	assert.NotNil(t, s)
+
+	orchestrator := New(s)
+	defer orchestrator.Stop()
+
+	watch, cancel := state.Watch(s.WatchQueue() /*state.EventCreateTask{}, state.EventUpdateTask{}*/)
+	defer cancel()
+
+	// Create a service with two instances specified before the orchestrator is
+	// started. This should result in two tasks when the orchestrator
+	// starts up.
+	err := s.Update(func(tx store.Tx) error {
+		j1 := &api.Service{
+			ID: "id1",
+			Spec: api.ServiceSpec{
+				Annotations: api.Annotations{
+					Name: "name1",
+				},
+				Instances: 2,
+				Mode:      api.ServiceModeRunning,
+				Restart: &api.RestartPolicy{
+					Condition:   api.RestartAlways,
+					Delay:       100 * time.Millisecond,
+					MaxAttempts: 1,
+				},
+			},
+		}
+		assert.NoError(t, store.CreateService(tx, j1))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Start the orchestrator.
+	go func() {
+		assert.NoError(t, orchestrator.Run(ctx))
+	}()
+
+	observedTask1 := watchTaskCreate(t, watch)
+	assert.Equal(t, observedTask1.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask1.Annotations.Name, "name1")
+
+	observedTask2 := watchTaskCreate(t, watch)
+	assert.Equal(t, observedTask2.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask2.Annotations.Name, "name1")
+
+	// Fail the first task. Confirm that it gets restarted.
+	updatedTask1 := observedTask1.Copy()
+	updatedTask1.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before := time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask1))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+
+	observedTask3 := watchTaskCreate(t, watch)
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask3.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask3.DesiredState, api.TaskStateReady)
+	assert.Equal(t, observedTask3.Annotations.Name, "name1")
+
+	observedTask4 := watchTaskUpdate(t, watch)
 	after := time.Now()
 
 	// At least 100 ms should have elapsed. Only check the lower bound,
@@ -362,5 +430,213 @@ func TestOrchestratorRestartDelay(t *testing.T) {
 	assert.Equal(t, observedTask4.DesiredState, api.TaskStateRunning)
 	assert.Equal(t, observedTask4.Annotations.Name, "name1")
 
-	orchestrator.Stop()
+	// Fail the second task. Confirm that it gets restarted.
+	updatedTask2 := observedTask2.Copy()
+	updatedTask2.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before = time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask2))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+
+	observedTask5 := watchTaskCreate(t, watch)
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask5.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask5.DesiredState, api.TaskStateReady)
+	assert.Equal(t, observedTask5.Annotations.Name, "name1")
+
+	observedTask6 := watchTaskUpdate(t, watch) // task gets started after a delay
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask6.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask6.DesiredState, api.TaskStateRunning)
+	assert.Equal(t, observedTask6.Annotations.Name, "name1")
+
+	// Fail the first instance again. It should not be restarted.
+	updatedTask1 = observedTask3.Copy()
+	updatedTask1.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before = time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask1))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+
+	select {
+	case <-watch:
+		t.Fatal("got unexpected event")
+	case <-time.After(200 * time.Millisecond):
+	}
+}
+
+func TestOrchestratorRestartWindow(t *testing.T) {
+	ctx := context.Background()
+	s := store.NewMemoryStore(nil)
+	assert.NotNil(t, s)
+
+	orchestrator := New(s)
+	defer orchestrator.Stop()
+
+	watch, cancel := state.Watch(s.WatchQueue() /*state.EventCreateTask{}, state.EventUpdateTask{}*/)
+	defer cancel()
+
+	// Create a service with two instances specified before the orchestrator is
+	// started. This should result in two tasks when the orchestrator
+	// starts up.
+	err := s.Update(func(tx store.Tx) error {
+		j1 := &api.Service{
+			ID: "id1",
+			Spec: api.ServiceSpec{
+				Annotations: api.Annotations{
+					Name: "name1",
+				},
+				Instances: 2,
+				Mode:      api.ServiceModeRunning,
+				Restart: &api.RestartPolicy{
+					Condition:   api.RestartAlways,
+					Delay:       100 * time.Millisecond,
+					MaxAttempts: 1,
+					Window:      500 * time.Millisecond,
+				},
+			},
+		}
+		assert.NoError(t, store.CreateService(tx, j1))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	// Start the orchestrator.
+	go func() {
+		assert.NoError(t, orchestrator.Run(ctx))
+	}()
+
+	observedTask1 := watchTaskCreate(t, watch)
+	assert.Equal(t, observedTask1.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask1.Annotations.Name, "name1")
+
+	observedTask2 := watchTaskCreate(t, watch)
+	assert.Equal(t, observedTask2.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask2.Annotations.Name, "name1")
+
+	// Fail the first task. Confirm that it gets restarted.
+	updatedTask1 := observedTask1.Copy()
+	updatedTask1.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before := time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask1))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+
+	observedTask3 := watchTaskCreate(t, watch)
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask3.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask3.DesiredState, api.TaskStateReady)
+	assert.Equal(t, observedTask3.Annotations.Name, "name1")
+
+	observedTask4 := watchTaskUpdate(t, watch)
+	after := time.Now()
+
+	// At least 100 ms should have elapsed. Only check the lower bound,
+	// because the system may be slow and it could have taken longer.
+	if after.Sub(before) < 100*time.Millisecond {
+		t.Fatal("restart delay should have elapsed")
+	}
+
+	assert.Equal(t, observedTask4.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask4.DesiredState, api.TaskStateRunning)
+	assert.Equal(t, observedTask4.Annotations.Name, "name1")
+
+	// Fail the second task. Confirm that it gets restarted.
+	updatedTask2 := observedTask2.Copy()
+	updatedTask2.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before = time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask2))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+
+	observedTask5 := watchTaskCreate(t, watch)
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask5.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask5.DesiredState, api.TaskStateReady)
+	assert.Equal(t, observedTask5.Annotations.Name, "name1")
+
+	observedTask6 := watchTaskUpdate(t, watch) // task gets started after a delay
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask6.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask6.DesiredState, api.TaskStateRunning)
+	assert.Equal(t, observedTask6.Annotations.Name, "name1")
+
+	// Fail the first instance again. It should not be restarted.
+	updatedTask1 = observedTask3.Copy()
+	updatedTask1.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before = time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask1))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+
+	select {
+	case <-watch:
+		t.Fatal("got unexpected event")
+	case <-time.After(200 * time.Millisecond):
+	}
+
+	time.Sleep(time.Second)
+
+	// Fail the second instance again. It should get restarted because
+	// enough time has elapsed since the last restarts.
+	updatedTask2 = observedTask5.Copy()
+	updatedTask2.Status = api.TaskStatus{State: api.TaskStateDead, TerminalState: api.TaskStateFailed}
+	before = time.Now()
+	err = s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.UpdateTask(tx, updatedTask2))
+		return nil
+	})
+	assert.NoError(t, err)
+	expectTaskUpdate(t, watch)
+	expectCommit(t, watch)
+	expectTaskUpdate(t, watch)
+
+	observedTask7 := watchTaskCreate(t, watch)
+	expectCommit(t, watch)
+	assert.Equal(t, observedTask7.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask7.DesiredState, api.TaskStateReady)
+	assert.Equal(t, observedTask7.Annotations.Name, "name1")
+
+	observedTask8 := watchTaskUpdate(t, watch)
+	after = time.Now()
+
+	// At least 100 ms should have elapsed. Only check the lower bound,
+	// because the system may be slow and it could have taken longer.
+	if after.Sub(before) < 100*time.Millisecond {
+		t.Fatal("restart delay should have elapsed")
+	}
+
+	assert.Equal(t, observedTask8.Status.State, api.TaskStateNew)
+	assert.Equal(t, observedTask8.DesiredState, api.TaskStateRunning)
+	assert.Equal(t, observedTask8.Annotations.Name, "name1")
 }
