5cc0f4e3f8c0db9e0a9808167eeae937a1cec84b
Andrea Luzzardi <aluzzardi@gmail.com>
2016-06-06 19:03:35 -0700

    api/cli: Misc improvements to node removal
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cmd/swarmctl/node/remove.go
+++ cmd/swarmctl/node/remove.go
@@ -1,11 +1,38 @@
 package node
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+
+	"github.com/docker/swarm-v2/api"
+	"github.com/docker/swarm-v2/cmd/swarmctl/common"
+	"github.com/spf13/cobra"
+)
 
 var (
 	removeCmd = &cobra.Command{
-		Use:   "remove <node ID>",
-		Short: "Remove a node",
-		RunE:  removeNode,
+		Use:     "remove <node ID>",
+		Short:   "Remove a node",
+		Aliases: []string{"rm"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				return errors.New("missing node ID")
+			}
+
+			c, err := common.Dial(cmd)
+			if err != nil {
+				return err
+			}
+			node, err := getNode(common.Context(cmd), c, args[0])
+			if err != nil {
+				return err
+			}
+
+			_, err = c.RemoveNode(common.Context(cmd), &api.RemoveNodeRequest{
+				NodeID:      node.ID,
+				NodeVersion: &node.Meta.Version,
+			})
+
+			return err
+		},
 	}
 )
