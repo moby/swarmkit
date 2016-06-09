ac2d5b9ffec7b9dff621653b7f837d0d421e7b6f
Drew Erny <drew.erny@docker.com>
2016-06-09 16:15:00 -0700

    Add regex to check if name is valid
    Restricts to the same names that are allowed for docker containers.
    Prevents users from creating services that cannot start because their
    names cannot be used as part of docker containers. prevents users from
    creating names that wouldn't also be valid DNS lables. fixes #897
    
    Signed-off-by: Drew Erny <drew.erny@docker.com>

--- manager/controlapi/common_test.go
+++ manager/controlapi/common_test.go
@@ -21,8 +21,25 @@ func TestValidateAnnotations(t *testing.T) {
 
 	for _, good := range []api.Annotations{
 		{Name: "name"},
+		{Name: "n-me"},
+		{Name: "n_me"},
+		{Name: "n-m-e"},
+		{Name: "n--d"},
 	} {
 		err := validateAnnotations(good)
-		assert.NoError(t, err)
+		assert.NoError(t, err, "string: "+good.Name)
+	}
+
+	for _, bad := range []api.Annotations{
+		{Name: "_nam"},
+		{Name: ".nam"},
+		{Name: "-nam"},
+		{Name: "nam-"},
+		{Name: "n/me"},
+		{Name: "n&me"},
+		{Name: "////"},
+	} {
+		err := validateAnnotations(bad)
+		assert.Error(t, err, "string: "+bad.Name)
 	}
 }
