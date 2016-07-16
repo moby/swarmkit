73afbe3db14a4a352dccdb8e4c333aad1be0c39d
Jana Radhakrishnan <mrjana@docker.com>
2016-07-15 23:11:43 -0700

    Track task/node allocation using ID
    Currently the task and node allocations are being tracked by the ip
    address allocated which is incorrect as it does not correctly specify
    that this IP is assigned to this task. Rather as is done for services,
    tasks and nodes allocation should be tracked by it's own ID in an
    allocation set. If the ID is present in the set then it is
    allocated. This avoids problems like double freeing the same IP which
    might have been allocated to other objects and cause conflicts.
    
    Signed-off-by: Jana Radhakrishnan <mrjana@docker.com>

--- manager/allocator/networkallocator/networkallocator.go
+++ manager/allocator/networkallocator/networkallocator.go
@@ -43,6 +43,14 @@ type NetworkAllocator struct {
 	// Allocator state to indicate if allocation has been
 	// successfully completed for this service.
 	services map[string]struct{}
+
+	// Allocator state to indicate if allocation has been
+	// successfully completed for this task.
+	tasks map[string]struct{}
+
+	// Allocator state to indicate if allocation has been
+	// successfully completed for this node.
+	nodes map[string]struct{}
 }
 
 // Local in-memory state related to netwok that need to be tracked by NetworkAllocator
@@ -64,6 +72,8 @@ func New() (*NetworkAllocator, error) {
 	na := &NetworkAllocator{
 		networks: make(map[string]*network),
 		services: make(map[string]struct{}),
+		tasks:    make(map[string]struct{}),
+		nodes:    make(map[string]struct{}),
 	}
 
 	// There are no driver configurations and notification
@@ -223,6 +233,12 @@ func (na *NetworkAllocator) IsAllocated(n *api.Network) bool {
 
 // IsTaskAllocated returns if the passed task has it's network resources allocated or not.
 func (na *NetworkAllocator) IsTaskAllocated(t *api.Task) bool {
+	// If the task is not found in the allocated set, then it is
+	// not allocated.
+	if _, ok := na.tasks[t.ID]; !ok {
+		return false
+	}
+
 	// If Networks is empty there is no way this Task is allocated.
 	if len(t.Networks) == 0 {
 		return false
@@ -267,6 +283,12 @@ func (na *NetworkAllocator) IsServiceAllocated(s *api.Service) bool {
 
 // IsNodeAllocated returns if the passed node has its network resources allocated or not.
 func (na *NetworkAllocator) IsNodeAllocated(node *api.Node) bool {
+	// If the node is not found in the allocated set, then it is
+	// not allocated.
+	if _, ok := na.nodes[node.ID]; !ok {
+		return false
+	}
+
 	// If no attachment, not allocated.
 	if node.Attachment == nil {
 		return false
@@ -294,12 +316,18 @@ func (na *NetworkAllocator) IsNodeAllocated(node *api.Node) bool {
 // AllocateNode allocates the IP addresses for the network to which
 // the node is attached.
 func (na *NetworkAllocator) AllocateNode(node *api.Node) error {
-	return na.allocateNetworkIPs(node.Attachment)
+	if err := na.allocateNetworkIPs(node.Attachment); err != nil {
+		return err
+	}
+
+	na.nodes[node.ID] = struct{}{}
+	return nil
 }
 
 // DeallocateNode deallocates the IP addresses for the network to
 // which the node is attached.
 func (na *NetworkAllocator) DeallocateNode(node *api.Node) error {
+	delete(na.nodes, node.ID)
 	return na.releaseEndpoints([]*api.NetworkAttachment{node.Attachment})
 }
 
@@ -315,12 +343,15 @@ func (na *NetworkAllocator) AllocateTask(t *api.Task) error {
 		}
 	}
 
+	na.tasks[t.ID] = struct{}{}
+
 	return nil
 }
 
 // DeallocateTask releases all the endpoint resources for all the
 // networks that a task is attached to.
 func (na *NetworkAllocator) DeallocateTask(t *api.Task) error {
+	delete(na.tasks, t.ID)
 	return na.releaseEndpoints(t.Networks)
 }
 
