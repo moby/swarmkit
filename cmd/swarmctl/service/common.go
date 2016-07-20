a654a6e9d85eb463b68402226d0b67767dff3d60
Doron Podoleanu <doronp@il.ibm.com>
2016-07-20 08:47:20 +0300

    Fixes#1105 Make service commands work with id prefix as well as name
    Signed-off-by: Doron Podoleanu <doronp@il.ibm.com>

--- cmd/swarmctl/service/common.go
+++ cmd/swarmctl/service/common.go
@@ -13,28 +13,63 @@ func getService(ctx context.Context, c api.ControlClient, input string) (*api.Se
 	rg, err := c.GetService(ctx, &api.GetServiceRequest{ServiceID: input})
 	if err != nil {
 		// If any error (including NotFound), ListServices to match via ID prefix and name prefix.
-		rl, err := c.ListServices(ctx,
-			&api.ListServicesRequest{
-				Filters: &api.ListServicesRequest_Filters{
-					NamePrefixes: []string{input},
-				},
-			},
-		)
+		service, err := getServiceByName(ctx, c, input)
 		if err != nil {
-			return nil, err
+			service, err = getServiceByPrefixedID(ctx, c, input)
+			if err != nil {
+				return nil, err
+			}
 		}
-
-		if len(rl.Services) == 0 {
-			return nil, fmt.Errorf("service %s not found", input)
-		}
-
-		if l := len(rl.Services); l > 1 {
-			return nil, fmt.Errorf("service %s is ambiguous (%d matches found)", input, l)
-		}
-
-		return rl.Services[0], nil
+		return service, nil
 	}
+
 	return rg.Service, nil
+}
+
+func getServiceByPrefixedID(ctx context.Context, c api.ControlClient, input string) (*api.Service, error) {
+	rl, err := c.ListServices(ctx,
+		&api.ListServicesRequest{
+			Filters: &api.ListServicesRequest_Filters{
+				IDPrefixes: []string{input},
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(rl.Services) == 0 {
+		return nil, fmt.Errorf("service %s not found", input)
+	}
+
+	if l := len(rl.Services); l > 1 {
+		return nil, fmt.Errorf("service %s is ambiguous (%d matches found)", input, l)
+	}
+
+	return rl.Services[0], nil
+}
+
+func getServiceByName(ctx context.Context, c api.ControlClient, input string) (*api.Service, error) {
+	rl, err := c.ListServices(ctx,
+		&api.ListServicesRequest{
+			Filters: &api.ListServicesRequest_Filters{
+				Names: []string{input},
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(rl.Services) == 0 {
+		return nil, fmt.Errorf("service %s not found", input)
+	}
+
+	if l := len(rl.Services); l > 1 {
+		return nil, fmt.Errorf("service %s is ambiguous (%d matches found)", input, l)
+	}
+
+	return rl.Services[0], nil
 }
 
 func getServiceReplicasTxt(s *api.Service, running int) string {
