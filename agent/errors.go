60b8b923538ab2bd03d0a58c62039616c52d229e
Stephen J Day <stephen.day@docker.com>
2016-07-29 15:44:02 -0700

    agent: simplify Start and Stop
    After finding a possible race condition, `Agent`/`Node` `Start` and
    `Stop` methods have been greatly simplified. While this race condition
    is very unlikely to occur in the wild, there are possible panics if the
    caller is not careful about calling `Start` and `Stop` from multiple
    goroutines.
    
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- agent/errors.go
+++ agent/errors.go
@@ -11,9 +11,7 @@ var (
 
 	errNodeNotRegistered = fmt.Errorf("node not registered")
 
-	errAgentNotStarted = errors.New("agent: not started")
-	errAgentStarted    = errors.New("agent: already started")
-	errAgentStopped    = errors.New("agent: stopped")
+	errAgentStarted = errors.New("agent: already started")
 
 	errTaskNoContoller          = errors.New("agent: no task controller")
 	errTaskNotAssigned          = errors.New("agent: task not assigned")
