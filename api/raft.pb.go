7cb749ca87fc5c8be08462fef9833dff8f8a0efc
Alexander Morozov <lk4d4math@gmail.com>
2016-08-09 22:06:54 -0700

    raftproxy: handle all errors
    Because proxy is both client and server it can receive errors wrapped as
    RPC errors or plain errors.
    
    I hope this time I covered them all. It covers:
    * ClientConn errors and their RPC wrapped versions
    * transport.ConnectionError ("connection error" string check)
    * RPC wrapped transport.ConnectionError (codes.Internal check)
    
    Signed-off-by: Alexander Morozov <lk4d4math@gmail.com>

--- api/raft.pb.go
+++ api/raft.pb.go
@@ -1497,7 +1497,11 @@ func (p *raftProxyRaftServer) ProcessRaftMessage(ctx context.Context, r *Process
 
 	defer func() {
 		if err != nil {
-			if _, ok := err.(transport.ConnectionError); ok || err == grpc.ErrClientConnClosing || err == grpc.ErrClientConnTimeout {
+			errStr := err.Error()
+			if strings.Contains(errStr, grpc.ErrClientConnClosing.Error()) ||
+				strings.Contains(errStr, grpc.ErrClientConnTimeout.Error()) ||
+				strings.Contains(errStr, "connection error") ||
+				grpc.Code(err) == codes.Internal {
 				p.connSelector.Reset()
 			}
 		}
@@ -1522,7 +1526,11 @@ func (p *raftProxyRaftServer) ResolveAddress(ctx context.Context, r *ResolveAddr
 
 	defer func() {
 		if err != nil {
-			if _, ok := err.(transport.ConnectionError); ok || err == grpc.ErrClientConnClosing || err == grpc.ErrClientConnTimeout {
+			errStr := err.Error()
+			if strings.Contains(errStr, grpc.ErrClientConnClosing.Error()) ||
+				strings.Contains(errStr, grpc.ErrClientConnTimeout.Error()) ||
+				strings.Contains(errStr, "connection error") ||
+				grpc.Code(err) == codes.Internal {
 				p.connSelector.Reset()
 			}
 		}
@@ -1592,7 +1600,11 @@ func (p *raftProxyRaftMembershipServer) Join(ctx context.Context, r *JoinRequest
 
 	defer func() {
 		if err != nil {
-			if _, ok := err.(transport.ConnectionError); ok || err == grpc.ErrClientConnClosing || err == grpc.ErrClientConnTimeout {
+			errStr := err.Error()
+			if strings.Contains(errStr, grpc.ErrClientConnClosing.Error()) ||
+				strings.Contains(errStr, grpc.ErrClientConnTimeout.Error()) ||
+				strings.Contains(errStr, "connection error") ||
+				grpc.Code(err) == codes.Internal {
 				p.connSelector.Reset()
 			}
 		}
@@ -1617,7 +1629,11 @@ func (p *raftProxyRaftMembershipServer) Leave(ctx context.Context, r *LeaveReque
 
 	defer func() {
 		if err != nil {
-			if _, ok := err.(transport.ConnectionError); ok || err == grpc.ErrClientConnClosing || err == grpc.ErrClientConnTimeout {
+			errStr := err.Error()
+			if strings.Contains(errStr, grpc.ErrClientConnClosing.Error()) ||
+				strings.Contains(errStr, grpc.ErrClientConnTimeout.Error()) ||
+				strings.Contains(errStr, "connection error") ||
+				grpc.Code(err) == codes.Internal {
 				p.connSelector.Reset()
 			}
 		}
