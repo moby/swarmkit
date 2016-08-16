38dddf55f28cde6390433887814da17ff704ac0a
Aaron Lehmann <aaron.lehmann@docker.com>
2016-08-16 15:22:22 -0700

    Add update order
    This adds a new UpdateOrder enum to the UpdateConfig. Two values are
    supported:
    
    - STOP_FIRST: The old behavior - wait for the old task to shut down
      before starting the new one.
    
    - START_FIRST: Start the new task, and wait for it to reach RUNNING
      before shutting down the old task.
    
    Comply with this flag in the updater, add a flag to swarmctl, and add a
    unit test.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- cmd/swarmctl/service/flagparser/update.go
+++ cmd/swarmctl/service/flagparser/update.go
@@ -58,6 +58,26 @@ func parseUpdate(flags *pflag.FlagSet, spec *api.ServiceSpec) error {
 		}
 	}
 
+	if flags.Changed("update-order") {
+		if spec.Update == nil {
+			spec.Update = &api.UpdateConfig{}
+		}
+
+		order, err := flags.GetString("update-order")
+		if err != nil {
+			return err
+		}
+
+		switch order {
+		case "stop-first":
+			spec.Update.Order = api.UpdateConfig_STOP_FIRST
+		case "start-first":
+			spec.Update.Order = api.UpdateConfig_START_FIRST
+		default:
+			return errors.New("--update-order value must be stop-first or start-first")
+		}
+	}
+
 	if flags.Changed("rollback-parallelism") {
 		parallelism, err := flags.GetUint64("rollback-parallelism")
 		if err != nil {
@@ -102,6 +122,26 @@ func parseUpdate(flags *pflag.FlagSet, spec *api.ServiceSpec) error {
 			spec.Rollback.FailureAction = api.UpdateConfig_CONTINUE
 		default:
 			return errors.New("--rollback-on-failure value must be pause or continue")
+		}
+	}
+
+	if flags.Changed("rollback-order") {
+		if spec.Rollback == nil {
+			spec.Rollback = &api.UpdateConfig{}
+		}
+
+		order, err := flags.GetString("rollback-order")
+		if err != nil {
+			return err
+		}
+
+		switch order {
+		case "stop-first":
+			spec.Rollback.Order = api.UpdateConfig_STOP_FIRST
+		case "start-first":
+			spec.Rollback.Order = api.UpdateConfig_START_FIRST
+		default:
+			return errors.New("--rollback-order value must be stop-first or start-first")
 		}
 	}
 
