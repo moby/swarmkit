0cb342e299287acfc6069122844b21c24a11d70a
allencloud <allen.sun@daocloud.io>
2016-09-14 11:24:34 +0800

    fix PublishedPort allocate logic
    Signed-off-by: allencloud <allen.sun@daocloud.io>

--- manager/allocator/networkallocator/portallocator.go
+++ manager/allocator/networkallocator/portallocator.go
@@ -209,18 +209,19 @@ func (pa *portAllocator) isPortsAllocated(s *api.Service) bool {
 
 func (ps *portSpace) allocate(p *api.PortConfig) (err error) {
 	if p.PublishedPort != 0 {
+
+		defer func() {
+			if err != nil {
+				ps.dynamicPortSpace.Release(uint64(p.PublishedPort))
+			}
+		}()
+
 		// If it falls in the dynamic port range check out
 		// from dynamic port space first.
 		if p.PublishedPort >= dynamicPortStart && p.PublishedPort <= dynamicPortEnd {
 			if err = ps.dynamicPortSpace.GetSpecificID(uint64(p.PublishedPort)); err != nil {
 				return err
 			}
-
-			defer func() {
-				if err != nil {
-					ps.dynamicPortSpace.Release(uint64(p.PublishedPort))
-				}
-			}()
 		}
 
 		return ps.masterPortSpace.GetSpecificID(uint64(p.PublishedPort))
@@ -231,6 +232,7 @@ func (ps *portSpace) allocate(p *api.PortConfig) (err error) {
 	if err != nil {
 		return
 	}
+
 	defer func() {
 		if err != nil {
 			ps.dynamicPortSpace.Release(uint64(swarmPort))
