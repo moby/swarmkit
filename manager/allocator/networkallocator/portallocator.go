a4bed1fb6381ed7107e4c251c5504e42005a5740
Yong Tang <yong.tang.github@outlook.com>
2016-12-09 07:18:45 -0800

    Fix duplicated ports allocation with restarted swarm
    This fix fixes the issue raised in:
    https://github.com/docker/docker/issues/29247
    
    where duplicated ports (`PublishedPort`) is being allocated
    with restarted swarm nodes.
    
    The reason for the issue was that `isPortsAllocated` relies
    on portSpace which does not capture the restored services when
    swarm is restarted.
    This fix add a flag in `isPortsAllocated` so that in case
    the allocator is still in initialization stage, dynamically
    allocated ports (`PublishedPort`) will always be reallocated.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    (cherry picked from commit 1f6ed97529d75c9eebbe2d65901d12c32cce2238)

--- manager/allocator/networkallocator/portallocator.go
+++ manager/allocator/networkallocator/portallocator.go
@@ -285,6 +285,10 @@ func (pa *portAllocator) portsAllocatedInHostPublishMode(s *api.Service) bool {
 }
 
 func (pa *portAllocator) isPortsAllocated(s *api.Service) bool {
+	return pa.isPortsAllocatedOnInit(s, false)
+}
+
+func (pa *portAllocator) isPortsAllocatedOnInit(s *api.Service, onInit bool) bool {
 	// If service has no user-defined endpoint and allocated endpoint,
 	// we assume it is allocated and return true.
 	if s.Endpoint == nil && s.Spec.Endpoint == nil {
@@ -340,6 +344,13 @@ func (pa *portAllocator) isPortsAllocated(s *api.Service) bool {
 		// is not initialized with a valid SwarmPort value then
 		// we are not allocated.
 		if portConfig.PublishedPort == 0 && portState.PublishedPort == 0 {
+			return false
+		}
+
+		// If SwarmPort was not defined by user and the func
+		// is called during allocator initialization state then
+		// we are not allocated.
+		if portConfig.PublishedPort == 0 && onInit {
 			return false
 		}
 	}
