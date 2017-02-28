fce4e267dfded5b5f62644124e0b821330268297
Aaron Lehmann <aaron.lehmann@docker.com>
2017-02-27 19:13:45 -0800

    integration: Improve logging
    Since integration tests tests run in parallel, it can be very hard to
    tell which test a log line came from.
    
    Add a "testname" field to the logger used for the tests.
    
    Plumb context through roleManager so that its log messages will benefit
    from this.
    
    Also preserve the logger in (*Node).run and (*Node).runManager so these
    injected fields reach further.
    
    I considered turning on debug logging, but it's an enormous amount of
    information.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -147,7 +147,18 @@ func pollServiceReady(t *testing.T, c *testCluster, sid string, replicas int) {
 }
 
 func newCluster(t *testing.T, numWorker, numManager int) *testCluster {
-	cl := newTestCluster()
+	// Get name of caller
+	var testName string
+	pc, _, _, ok := runtime.Caller(1)
+	if ok {
+		funcName := runtime.FuncForPC(pc).Name()
+		splitted := strings.Split(funcName, ".")
+		if len(splitted) > 1 {
+			testName = splitted[len(splitted)-1]
+		}
+	}
+
+	cl := newTestCluster(testName)
 	for i := 0; i < numManager; i++ {
 		require.NoError(t, cl.AddManager(false, nil), "manager number %d", i+1)
 	}
@@ -174,7 +185,7 @@ func TestServiceCreateLateBind(t *testing.T) {
 
 	numWorker, numManager := 3, 3
 
-	cl := newTestCluster()
+	cl := newTestCluster("TestServiceCreateLateBind")
 	for i := 0; i < numManager; i++ {
 		require.NoError(t, cl.AddManager(true, nil), "manager number %d", i+1)
 	}
@@ -489,7 +500,7 @@ func TestForceNewCluster(t *testing.T) {
 
 	// start a new cluster with the external CA bootstrapped
 	numWorker, numManager := 0, 1
-	cl := newTestCluster()
+	cl := newTestCluster("TestForceNewCluster")
 	defer func() {
 		require.NoError(t, cl.Stop())
 	}()
