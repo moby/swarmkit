9745dafd4c506e4ebf8bff643d6d79d6aceae282
cyli <ying.li@docker.com>
2017-04-07 14:00:31 -0700

    Include TLS information when listing and inspecting nodes
    Signed-off-by: cyli <ying.li@docker.com>

--- cmd/swarmctl/node/list.go
+++ cmd/swarmctl/node/list.go
@@ -6,7 +6,10 @@ import (
 	"os"
 	"text/tabwriter"
 
+	"reflect"
+
 	"github.com/docker/swarmkit/api"
+	"github.com/docker/swarmkit/ca"
 	"github.com/docker/swarmkit/cmd/swarmctl/common"
 	"github.com/spf13/cobra"
 )
@@ -38,13 +41,27 @@ var (
 
 			var output func(n *api.Node)
 
+			cluster, err := getCluster(common.Context(cmd), c)
+			if err != nil {
+				return err
+			}
+			clusterIssuer, err := ca.IssuerFromAPIRootCA(&cluster.RootCA)
+			if err != nil {
+				return err
+			}
+			desiredTLSInfo := &api.NodeTLSInfo{
+				CertIssuerPublicKey: clusterIssuer.PublicKey,
+				CertIssuerSubject:   clusterIssuer.Subject,
+				TrustRoot:           cluster.RootCA.CACert,
+			}
+
 			if !quiet {
 				w := tabwriter.NewWriter(os.Stdout, 0, 4, 2, ' ', 0)
 				defer func() {
 					// Ignore flushing errors - there's nothing we can do.
 					_ = w.Flush()
 				}()
-				common.PrintHeader(w, "ID", "Name", "Membership", "Status", "Availability", "Manager Status")
+				common.PrintHeader(w, "ID", "Name", "Membership", "Status", "Availability", "Manager Status", "TLS Status")
 				output = func(n *api.Node) {
 					spec := &n.Spec
 					name := spec.Annotations.Name
@@ -64,14 +81,21 @@ var (
 					if reachability == "" && spec.DesiredRole == api.NodeRoleManager {
 						reachability = "UNKNOWN"
 					}
+					tlsStatus := "OUTDATED"
+					if n.Description == nil || n.Description.TLSInfo == nil {
+						tlsStatus = "UNKNOWN"
+					} else if reflect.DeepEqual(n.Description.TLSInfo, desiredTLSInfo) {
+						tlsStatus = "CURRENT"
+					}
 
-					fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n",
+					fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
 						n.ID,
 						name,
 						membership,
 						n.Status.State.String(),
 						availability,
 						reachability,
+						tlsStatus,
 					)
 				}
 			} else {
