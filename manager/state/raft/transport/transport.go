8c85fddb41a1a68abcee1e62e028bc2d04ae1b2a
Anshul Pundir <anshul.x.pundir@gmail.com>
2017-10-24 13:11:03 -0700

    Provide custom gRPC dialer to override default proxy dialer (#2419)
    Signed-off-by: Anshul Pundir <anshul.pundir@docker.com>
    (cherry picked from commit 312be5983e2d2f01f0cbcc99d027b28433256705)
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

--- manager/state/raft/transport/transport.go
+++ manager/state/raft/transport/transport.go
@@ -3,6 +3,7 @@
 package transport
 
 import (
+	"net"
 	"sync"
 	"time"
 
@@ -346,6 +347,13 @@ func (t *Transport) dial(addr string) (*grpc.ClientConn, error) {
 	if t.config.SendTimeout > 0 {
 		grpcOptions = append(grpcOptions, grpc.WithTimeout(t.config.SendTimeout))
 	}
+
+	// gRPC dialer connects to proxy first. Provide a custom dialer here avoid that.
+	// TODO(anshul) Add an option to configure this.
+	grpcOptions = append(grpcOptions,
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("tcp", addr, timeout)
+		}))
 
 	cc, err := grpc.Dial(addr, grpcOptions...)
 	if err != nil {
