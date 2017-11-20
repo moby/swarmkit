58e42ec9f7f3d241966b1d0f7bc34457888f6aa4
Anshul Pundir <anshul.pundir@docker.com>
2017-11-20 11:52:03 -0800

    Protoc changes for raft message streaming.
    Signed-off-by: Anshul Pundir <anshul.pundir@docker.com>

--- api/ca.pb.go
+++ api/ca.pb.go
@@ -131,6 +131,8 @@
 		LeaveResponse
 		ProcessRaftMessageRequest
 		ProcessRaftMessageResponse
+		StreamRaftMessageRequest
+		StreamRaftMessageResponse
 		ResolveAddressRequest
 		ResolveAddressResponse
 		InternalRaftRequest
