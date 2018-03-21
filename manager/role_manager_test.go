64431494941cb2a701dcaf2f4c952cc88895cde7
Ying Li <ying.li@docker.com>
2018-03-21 15:59:15 -0700

    Also delete nodes from the raft cluster if deletions come in, since a node that is a manager can be removed if it hasn't joined the raft cluster yet.  However, perhaps the raft conf change happens right after the check.
    Signed-off-by: Ying Li <ying.li@docker.com>
    (cherry picked from commit 36954b5dc780db3ba4b494b61e016ed6f1c0f9fa)
    Signed-off-by: Ying Li <ying.li@docker.com>

--- manager/role_manager_test.go
+++ manager/role_manager_test.go
@@ -10,12 +10,12 @@ import (
 
 	"google.golang.org/grpc/grpclog"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/docker/swarmkit/api"
 	cautils "github.com/docker/swarmkit/ca/testutils"
 	raftutils "github.com/docker/swarmkit/manager/state/raft/testutils"
 	"github.com/docker/swarmkit/manager/state/store"
 	"github.com/docker/swarmkit/testutils"
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
@@ -79,8 +79,14 @@ func TestRoleManagerRemovesDemotedNodesAndAddsPromotedNodes(t *testing.T) {
 		return store.UpdateNode(tx, n)
 	}))
 	require.NoError(t, testutils.PollFuncWithTimeout(fc, func() error {
-		if len(lead.GetMemberlist()) != 2 {
+		memberlist := lead.GetMemberlist()
+		if len(memberlist) != 2 {
 			return errors.New("raft node hasn't been removed yet")
+		}
+		for _, m := range memberlist {
+			if m.NodeID == nonLead.SecurityConfig.ClientTLSCreds.NodeID() {
+				return errors.New("wrong member was removed")
+			}
 		}
 		// use Update just because it returns an error
 		return lead.MemoryStore().Update(func(tx store.Tx) error {
@@ -154,8 +160,14 @@ func TestRoleManagerRemovesDemotedNodesOnStartup(t *testing.T) {
 	defer rm.Stop()
 
 	require.NoError(t, testutils.PollFuncWithTimeout(fc, func() error {
-		if len(lead.GetMemberlist()) != 2 {
+		memberlist := lead.GetMemberlist()
+		if len(memberlist) != 2 {
 			return errors.New("raft node hasn't been removed yet")
+		}
+		for _, m := range memberlist {
+			if m.NodeID == demoted.SecurityConfig.ClientTLSCreds.NodeID() {
+				return errors.New("wrong member was removed")
+			}
 		}
 		// use Update just because it returns an error
 		return lead.MemoryStore().Update(func(tx store.Tx) error {
@@ -165,6 +177,68 @@ func TestRoleManagerRemovesDemotedNodesOnStartup(t *testing.T) {
 			return nil
 		})
 	}, roleReconcileInterval/2))
+}
+
+// While roleManager is running, if a node is deleted, it is removed from the raft cluster.
+func TestRoleManagerRemovesDeletedNodes(t *testing.T) {
+	t.Parallel()
+
+	tc := cautils.NewTestCA(nil)
+	defer tc.Stop()
+
+	nodes, fc := raftutils.NewRaftCluster(t, tc)
+	defer raftutils.TeardownCluster(nodes)
+
+	// nodes is not a list, but a map.  The IDs are 1, 2, 3
+	require.Len(t, nodes[1].GetMemberlist(), 3)
+
+	// create node objects in the memory store
+	for _, node := range nodes {
+		s := raftutils.Leader(nodes).MemoryStore()
+		// Create a new node object
+		require.NoError(t, s.Update(func(tx store.Tx) error {
+			return store.CreateNode(tx, &api.Node{
+				Role: api.NodeRoleManager,
+				ID:   node.SecurityConfig.ClientTLSCreds.NodeID(),
+				Spec: api.NodeSpec{
+					DesiredRole:  api.NodeRoleManager,
+					Membership:   api.NodeMembershipAccepted,
+					Availability: api.NodeAvailabilityActive,
+				},
+			})
+		}))
+	}
+
+	lead := raftutils.Leader(nodes)
+	var nonLead *raftutils.TestNode
+	for _, n := range nodes {
+		if n != lead {
+			nonLead = n
+			break
+		}
+	}
+	rm := newRoleManager(lead.MemoryStore(), lead.Node)
+	rm.clocksource = fc
+	go rm.Run(tc.Context)
+	defer rm.Stop()
+
+	// delete the node
+	require.NoError(t, lead.MemoryStore().Update(func(tx store.Tx) error {
+		return store.DeleteNode(tx, nonLead.SecurityConfig.ClientTLSCreds.NodeID())
+	}))
+	require.NoError(t, testutils.PollFuncWithTimeout(fc, func() error {
+		memberlist := lead.GetMemberlist()
+		if len(memberlist) != 2 {
+			return errors.New("raft node hasn't been removed yet")
+		}
+		for _, m := range memberlist {
+			if m.NodeID == nonLead.SecurityConfig.ClientTLSCreds.NodeID() {
+				return errors.New("wrong member was removed")
+			}
+		}
+		return nil
+	}, roleReconcileInterval/2))
+
 }
 
 // If a node was removed before the roleManager starts up, roleManger will remove
@@ -208,8 +282,14 @@ func TestRoleManagerRemovesDeletedNodesOnStartup(t *testing.T) {
 	defer rm.Stop()
 
 	require.NoError(t, testutils.PollFuncWithTimeout(fc, func() error {
-		if len(lead.GetMemberlist()) != 2 {
+		memberlist := lead.GetMemberlist()
+		if len(memberlist) != 2 {
 			return errors.New("raft node hasn't been removed yet")
+		}
+		for _, m := range memberlist {
+			if m.NodeID == nodes[3].SecurityConfig.ClientTLSCreds.NodeID() {
+				return errors.New("wrong member was removed")
+			}
 		}
 		return nil
 	}, roleReconcileInterval/2))
