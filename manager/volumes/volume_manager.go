b0a1aa66decad17f9be00ecdfcff2f457ddcf10f
Drew Erny <derny@mirantis.com>
2020-07-20 11:05:12 -0500

    Add code for creating volumes
    Adds code for creating CSI volumes. This includes:
    
    * The basic Plugin object, which manages the connection to the CSI
    plugin
    * The basic VolumeManager object, which manages plugins and responds to
    store events
    
    This also includes lots of tests and tests rigging, including fake CSI
    clients.
    
    Signed-off-by: Drew Erny <derny@mirantis.com>

---
+++ manager/volumes/volume_manager.go
@@ -0,0 +1,159 @@
+package volumes
+
+import (
+	"context"
+	"sync"
+
+	"github.com/docker/go-events"
+
+	"github.com/docker/swarmkit/api"
+	"github.com/docker/swarmkit/manager/state/store"
+	// "github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+type VolumeManager struct {
+	store *store.MemoryStore
+	// provider is the SecretProvider which allows retrieving secrets. Used
+	// when creating new Plugin objects.
+	provider SecretProvider
+
+	// newPlugin is a function which returns an object implementing the Plugin
+	// interface. It allows us to swap out the implementation of plugins while
+	// unit-testing the VolumeManager
+	newPlugin func(config *api.CSIConfig_Plugin, provider SecretProvider) Plugin
+
+	// synchronization for starting and stopping the VolumeManager
+	startOnce sync.Once
+
+	stopChan chan struct{}
+	stopOnce sync.Once
+	doneChan chan struct{}
+
+	cluster *api.Cluster
+	plugins map[string]Plugin
+}
+
+func NewVolumeManager(s *store.MemoryStore) *VolumeManager {
+	return &VolumeManager{
+		store:     s,
+		stopChan:  make(chan struct{}),
+		doneChan:  make(chan struct{}),
+		newPlugin: NewPlugin,
+		plugins:   map[string]Plugin{},
+		provider:  NewSecretProvider(s),
+	}
+}
+
+func (vm *VolumeManager) Run() {
+	vm.startOnce.Do(func() {
+		vm.run()
+	})
+}
+
+func (vm *VolumeManager) run() {
+	defer close(vm.doneChan)
+
+	watch, cancel, err := store.ViewAndWatch(vm.store, func(tx store.ReadTx) error {
+		cluster, err := store.FindClusters(tx, store.ByName(store.DefaultClusterName))
+		if err != nil {
+			return err
+		}
+		vm.cluster = cluster[0]
+		return nil
+	})
+	if err != nil {
+		// TODO(dperny): log message
+		return
+	}
+	defer cancel()
+
+	vm.init()
+
+	for {
+		select {
+		case ev := <-watch:
+			vm.handleEvent(ev)
+		case <-vm.stopChan:
+			return
+		}
+	}
+}
+
+// init does one-time setup work for the VolumeManager, like creating all of
+// the Plugins and initializing the local state of the component.
+func (vm *VolumeManager) init() {
+	vm.updatePlugins()
+}
+
+func (vm *VolumeManager) updatePlugins() {
+	// activePlugins is a set of plugin names that are currently in the cluster
+	// spec. this lets remove from the vm.plugins map any plugins that are
+	// no longer in use.
+	activePlugins := map[string]struct{}{}
+
+	if vm.cluster != nil {
+		for _, plugin := range vm.cluster.Spec.CSIConfig.Plugins {
+			// it's exceedingly unlikely that plugin could ever be nil but
+			// better this than segfault
+			if plugin != nil {
+				if _, ok := vm.plugins[plugin.Name]; !ok {
+					vm.plugins[plugin.Name] = vm.newPlugin(plugin, vm.provider)
+				}
+				activePlugins[plugin.Name] = struct{}{}
+			}
+		}
+	}
+
+	// remove any plugins that are no longer in use.
+	for pluginName := range vm.plugins {
+		if _, ok := activePlugins[pluginName]; !ok {
+			delete(vm.plugins, pluginName)
+		}
+	}
+}
+
+func (vm *VolumeManager) Stop() {
+	vm.stopOnce.Do(func() {
+		close(vm.stopChan)
+	})
+
+	<-vm.doneChan
+}
+
+func (vm *VolumeManager) handleEvent(ev events.Event) {
+	switch e := ev.(type) {
+	case api.EventUpdateCluster:
+		// TODO(dperny): verify that the Cluster in this event can never be nil
+		vm.cluster = e.Cluster
+		vm.updatePlugins()
+	case api.EventCreateVolume:
+		vm.createVolume(e.Volume)
+	}
+}
+
+func (vm *VolumeManager) createVolume(v *api.Volume) {
+	p, ok := vm.plugins[v.Spec.Driver.Name]
+	if !ok {
+		// TODO(dperny): log something
+		return
+	}
+
+	info, err := p.CreateVolume(context.Background(), v)
+	if err != nil {
+		// TODO(dperny) log or handle errors
+		return
+	}
+
+	// TODO(dperny): handle error
+	vm.store.Update(func(tx store.Tx) error {
+		v2 := store.GetVolume(tx, v.ID)
+		// TODO(dperny): handle missing volume
+		if v2 == nil {
+			return nil
+		}
+
+		v2.VolumeInfo = info
+
+		return store.UpdateVolume(tx, v2)
+	})
+}
