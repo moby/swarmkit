dperny/swarmkit-1

Add plugin calls to RPCs
Adds code to the csi plugin adapter to actually call the underlying CSI
RPCs for ControllerPublishVolume, ControllerUnpublishVolume, and
DeleteVolume.

manager/csi/fakes_test.go
@@ -3,6 +3,7 @@ package csi
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
@@ -12,9 +13,16 @@ import (
 	"github.com/docker/swarmkit/api"
 )
 
-// failDeleteLabel is a label set on a Volume to cause DeleteVolume on the fake
-// plugin to fail
-const failDeleteLabel = "fakes_fail_delete"
+const (
+	// failDeleteLabel is a label set on a Volume to cause DeleteVolume on the
+	// fake plugin to fail
+	failDeleteLabel = "fakes_fail_delete"
+
+	// failPublishLabel is a label set on a Volume to cause PublishVolume or
+	// UnpublishVolume to fail. The value of the label is the Node ID to fail
+	// on.
+	failPublishLabel = "fakes_fail_publish"
+)
 
 // volumes_fakes_test.go includes the fakes for unit-testing parts of the
 // volumes code.
@@ -81,6 +89,10 @@ type fakeControllerClient struct {
 	createVolumeRequests []*csi.CreateVolumeRequest
 	// publishRequests is a log of all requests to ControllerPublishVolume
 	publishRequests []*csi.ControllerPublishVolumeRequest
+	// unpublishRequests is a log of all requests to ControllerUnpublishVolume
+	unpublishRequests []*csi.ControllerUnpublishVolumeRequest
+	// deleteRequests is a log of all requests to DeleteVolume
+	deleteRequests []*csi.DeleteVolumeRequest
 	// idCounter is a simple way to generate ids
 	idCounter int
 }
@@ -117,7 +129,9 @@ func (f *fakeControllerClient) CreateVolume(ctx context.Context, in *csi.CreateV
 }
 
 func (f *fakeControllerClient) DeleteVolume(ctx context.Context, in *csi.DeleteVolumeRequest, _ ...grpc.CallOption) (*csi.DeleteVolumeResponse, error) {
-	return nil, nil
+	f.deleteRequests = append(f.deleteRequests, in)
+	// deleteVolumeResponse intentionally left blank
+	return &csi.DeleteVolumeResponse{}, nil
 }
 
 func (f *fakeControllerClient) ControllerPublishVolume(ctx context.Context, in *csi.ControllerPublishVolumeRequest, _ ...grpc.CallOption) (*csi.ControllerPublishVolumeResponse, error) {
@@ -131,7 +145,8 @@ func (f *fakeControllerClient) ControllerPublishVolume(ctx context.Context, in *
 }
 
 func (f *fakeControllerClient) ControllerUnpublishVolume(ctx context.Context, in *csi.ControllerUnpublishVolumeRequest, _ ...grpc.CallOption) (*csi.ControllerUnpublishVolumeResponse, error) {
-	return nil, nil
+	f.unpublishRequests = append(f.unpublishRequests, in)
+	return &csi.ControllerUnpublishVolumeResponse{}, nil
 }
 
 func (f *fakeControllerClient) ValidateVolumeCapabilities(ctx context.Context, in *csi.ValidateVolumeCapabilitiesRequest, _ ...grpc.CallOption) (*csi.ValidateVolumeCapabilitiesResponse, error) {
@@ -176,13 +191,14 @@ func (fpm *fakePluginMaker) newFakePlugin(config *api.CSIConfig_Plugin, provider
 	fpm.Lock()
 	defer fpm.Unlock()
 	p := &fakePlugin{
-		name:             config.Name,
-		socket:           config.Socket,
-		swarmToCSI:       map[string]string{},
-		volumesCreated:   map[string]*api.Volume{},
-		volumesDeleted:   []string{},
-		volumesPublished: map[string][]string{},
-		removedIDs:       map[string]struct{}{},
+		name:               config.Name,
+		socket:             config.Socket,
+		swarmToCSI:         map[string]string{},
+		volumesCreated:     map[string]*api.Volume{},
+		volumesDeleted:     []string{},
+		volumesPublished:   map[string][]string{},
+		volumesUnpublished: map[string][]string{},
+		removedIDs:         map[string]struct{}{},
 	}
 	fpm.plugins[config.Name] = p
 	return p
@@ -202,6 +218,9 @@ type fakePlugin struct {
 	// volumesPublished maps the ID of a Volume to the Nodes it was published
 	// to
 	volumesPublished map[string][]string
+	// volumesUnpublished maps the ID of a volume to the Nodes it has been
+	// unpublished from
+	volumesUnpublished map[string][]string
 }
 
 func (f *fakePlugin) CreateVolume(ctx context.Context, v *api.Volume) (*api.VolumeInfo, error) {
@@ -224,11 +243,25 @@ func (f *fakePlugin) DeleteVolume(ctx context.Context, v *api.Volume) error {
 }
 
 func (f *fakePlugin) PublishVolume(ctx context.Context, v *api.Volume, nodeID string) (map[string]string, error) {
+	if fail, ok := v.Spec.Annotations.Labels[failPublishLabel]; ok {
+		if strings.Contains(fail, nodeID) {
+			return nil, fmt.Errorf("failing publish on %s since the label is set", nodeID)
+		}
+	}
 	f.volumesPublished[v.ID] = append(f.volumesPublished[v.ID], nodeID)
-	// TODO(dperny): return somethign here
 	return map[string]string{
 		"faked": "yeah",
 	}, nil
+}
+
+func (f *fakePlugin) UnpublishVolume(ctx context.Context, v *api.Volume, nodeID string) error {
+	if fail, ok := v.Spec.Annotations.Labels[failPublishLabel]; ok {
+		if strings.Contains(fail, nodeID) {
+			return fmt.Errorf("failing unpublish on %s since the label is set", nodeID)
+		}
+	}
+	f.volumesUnpublished[v.ID] = append(f.volumesUnpublished[v.ID], nodeID)
+	return nil
 }
 
 func (f *fakePlugin) AddNode(swarmID, csiID string) {

moby/swarmkit

Signed-off-by: Drew Erny <derny@mirantis.com>

manager/csi/fakes_test.go
@@ -3,6 +3,7 @@ package csi
 import (
 	"context"
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
@@ -12,9 +13,16 @@ import (
 	"github.com/docker/swarmkit/api"
 )
 
-// failDeleteLabel is a label set on a Volume to cause DeleteVolume on the fake
-// plugin to fail
-const failDeleteLabel = "fakes_fail_delete"
+const (
+	// failDeleteLabel is a label set on a Volume to cause DeleteVolume on the
+	// fake plugin to fail
+	failDeleteLabel = "fakes_fail_delete"
+
+	// failPublishLabel is a label set on a Volume to cause PublishVolume or
+	// UnpublishVolume to fail. The value of the label is the Node ID to fail
+	// on.
+	failPublishLabel = "fakes_fail_publish"
+)
 
 // volumes_fakes_test.go includes the fakes for unit-testing parts of the
 // volumes code.
@@ -81,6 +89,10 @@ type fakeControllerClient struct {
 	createVolumeRequests []*csi.CreateVolumeRequest
 	// publishRequests is a log of all requests to ControllerPublishVolume
 	publishRequests []*csi.ControllerPublishVolumeRequest
+	// unpublishRequests is a log of all requests to ControllerUnpublishVolume
+	unpublishRequests []*csi.ControllerUnpublishVolumeRequest
+	// deleteRequests is a log of all requests to DeleteVolume
+	deleteRequests []*csi.DeleteVolumeRequest
 	// idCounter is a simple way to generate ids
 	idCounter int
 }
@@ -117,7 +129,9 @@ func (f *fakeControllerClient) CreateVolume(ctx context.Context, in *csi.CreateV
 }
 
 func (f *fakeControllerClient) DeleteVolume(ctx context.Context, in *csi.DeleteVolumeRequest, _ ...grpc.CallOption) (*csi.DeleteVolumeResponse, error) {
-	return nil, nil
+	f.deleteRequests = append(f.deleteRequests, in)
+	// deleteVolumeResponse intentionally left blank
+	return &csi.DeleteVolumeResponse{}, nil
 }
 
 func (f *fakeControllerClient) ControllerPublishVolume(ctx context.Context, in *csi.ControllerPublishVolumeRequest, _ ...grpc.CallOption) (*csi.ControllerPublishVolumeResponse, error) {
@@ -131,7 +145,8 @@ func (f *fakeControllerClient) ControllerPublishVolume(ctx context.Context, in *
 }
 
 func (f *fakeControllerClient) ControllerUnpublishVolume(ctx context.Context, in *csi.ControllerUnpublishVolumeRequest, _ ...grpc.CallOption) (*csi.ControllerUnpublishVolumeResponse, error) {
-	return nil, nil
+	f.unpublishRequests = append(f.unpublishRequests, in)
+	return &csi.ControllerUnpublishVolumeResponse{}, nil
 }
 
 func (f *fakeControllerClient) ValidateVolumeCapabilities(ctx context.Context, in *csi.ValidateVolumeCapabilitiesRequest, _ ...grpc.CallOption) (*csi.ValidateVolumeCapabilitiesResponse, error) {
@@ -176,13 +191,14 @@ func (fpm *fakePluginMaker) newFakePlugin(config *api.CSIConfig_Plugin, provider
 	fpm.Lock()
 	defer fpm.Unlock()
 	p := &fakePlugin{
-		name:             config.Name,
-		socket:           config.ControllerSocket,
-		swarmToCSI:       map[string]string{},
-		volumesCreated:   map[string]*api.Volume{},
-		volumesDeleted:   []string{},
-		volumesPublished: map[string][]string{},
-		removedIDs:       map[string]struct{}{},
+		name:               config.Name,
+		socket:             config.ControllerSocket,
+		swarmToCSI:         map[string]string{},
+		volumesCreated:     map[string]*api.Volume{},
+		volumesDeleted:     []string{},
+		volumesPublished:   map[string][]string{},
+		volumesUnpublished: map[string][]string{},
+		removedIDs:         map[string]struct{}{},
 	}
 	fpm.plugins[config.Name] = p
 	return p
@@ -202,6 +218,9 @@ type fakePlugin struct {
 	// volumesPublished maps the ID of a Volume to the Nodes it was published
 	// to
 	volumesPublished map[string][]string
+	// volumesUnpublished maps the ID of a volume to the Nodes it has been
+	// unpublished from
+	volumesUnpublished map[string][]string
 }
 
 func (f *fakePlugin) CreateVolume(ctx context.Context, v *api.Volume) (*api.VolumeInfo, error) {
@@ -224,11 +243,25 @@ func (f *fakePlugin) DeleteVolume(ctx context.Context, v *api.Volume) error {
 }
 
 func (f *fakePlugin) PublishVolume(ctx context.Context, v *api.Volume, nodeID string) (map[string]string, error) {
+	if fail, ok := v.Spec.Annotations.Labels[failPublishLabel]; ok {
+		if strings.Contains(fail, nodeID) {
+			return nil, fmt.Errorf("failing publish on %s since the label is set", nodeID)
+		}
+	}
 	f.volumesPublished[v.ID] = append(f.volumesPublished[v.ID], nodeID)
-	// TODO(dperny): return somethign here
 	return map[string]string{
 		"faked": "yeah",
 	}, nil
+}
+
+func (f *fakePlugin) UnpublishVolume(ctx context.Context, v *api.Volume, nodeID string) error {
+	if fail, ok := v.Spec.Annotations.Labels[failPublishLabel]; ok {
+		if strings.Contains(fail, nodeID) {
+			return fmt.Errorf("failing unpublish on %s since the label is set", nodeID)
+		}
+	}
+	f.volumesUnpublished[v.ID] = append(f.volumesUnpublished[v.ID], nodeID)
+	return nil
 }
 
 func (f *fakePlugin) AddNode(swarmID, csiID string) {