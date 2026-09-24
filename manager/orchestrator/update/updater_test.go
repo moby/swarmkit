moby/swarmkit

commit 47ddece4c66bcafc0563bedcc2d5db77033da003

api: Re-introduce TaskSpec.
Motivation: Some properties such as restart policy and placement do not
belong in the runtime. However, they are task specific therefore they do
not belong to the service either.

Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

manager/orchestrator/restart.go
@@ -43,6 +43,7 @@ type RestartSupervisor struct {
 	delays           map[string]delayedStart
 	history          map[instanceTuple]*instanceRestartInfo
 	historyByService map[string]map[instanceTuple]struct{}
+	taskTimeout      time.Duration
 }
 
 // NewRestartSupervisor creates a new RestartSupervisor.
@@ -52,12 +53,15 @@ func NewRestartSupervisor(store *store.MemoryStore) *RestartSupervisor {
 		delays:           make(map[string]delayedStart),
 		history:          make(map[instanceTuple]*instanceRestartInfo),
 		historyByService: make(map[string]map[instanceTuple]struct{}),
+		taskTimeout:      defaultOldTaskTimeout,
 	}
 }
 
 // Restart initiates a new task to replace t if appropriate under the service's
 // restart policy.
 func (r *RestartSupervisor) Restart(ctx context.Context, tx store.Tx, service *api.Service, t api.Task) error {
+	// TODO(aluzzardi): This function should not depend on `service`.
+
 	t.DesiredState = api.TaskStateShutdown
 	err := store.UpdateTask(tx, &t)
 	if err != nil {
@@ -88,9 +92,9 @@ func (r *RestartSupervisor) Restart(ctx context.Context, tx store.Tx, service *a
 	var restartDelay time.Duration
 	// Restart delay does not applied to drained nodes
 	if n == nil || n.Spec.Availability != api.NodeAvailabilityDrain {
-		if service.Spec.Restart != nil && service.Spec.Restart.Delay != nil {
+		if t.Spec.Restart != nil && t.Spec.Restart.Delay != nil {
 			var err error
-			restartDelay, err = ptypes.Duration(service.Spec.Restart.Delay)
+			restartDelay, err = ptypes.Duration(t.Spec.Restart.Delay)
 			if err != nil {
 				log.G(ctx).WithError(err).Error("invalid restart delay; using default")
 				restartDelay = defaultRestartDelay
@@ -113,21 +117,23 @@ func (r *RestartSupervisor) Restart(ctx context.Context, tx store.Tx, service *a
 		return err
 	}
 
-	r.recordRestartHistory(restartTask, service)
+	r.recordRestartHistory(restartTask)
 
-	r.DelayStart(ctx, tx, service, &t, restartTask.ID, restartDelay, waitStop)
+	r.DelayStart(ctx, tx, &t, restartTask.ID, restartDelay, waitStop)
 	return nil
 }
 
 func (r *RestartSupervisor) shouldRestart(ctx context.Context, t *api.Task, service *api.Service) bool {
-	condition := restartCondition(service)
+	// TODO(aluzzardi): This function should not depend on `service`.
+
+	condition := restartCondition(t)
 
 	if condition != api.RestartOnAny &&
 		(condition != api.RestartOnFailure || t.Status.State == api.TaskStateCompleted) {
 		return false
 	}
 
-	if service.Spec.Restart == nil || service.Spec.Restart.MaxAttempts == 0 {
+	if t.Spec.Restart == nil || t.Spec.Restart.MaxAttempts == 0 {
 		return true
 	}
 
@@ -150,18 +156,18 @@ func (r *RestartSupervisor) shouldRestart(ctx context.Context, t *api.Task, serv
 		return true
 	}
 
-	if service.Spec.Restart.Window == nil || (service.Spec.Restart.Window.Seconds == 0 && service.Spec.Restart.Window.Nanos == 0) {
-		return restartInfo.totalRestarts < service.Spec.Restart.MaxAttempts
+	if t.Spec.Restart.Window == nil || (t.Spec.Restart.Window.Seconds == 0 && t.Spec.Restart.Window.Nanos == 0) {
+		return restartInfo.totalRestarts < t.Spec.Restart.MaxAttempts
 	}
 
 	if restartInfo.restartedInstances == nil {
 		return true
 	}
 
-	window, err := ptypes.Duration(service.Spec.Restart.Window)
+	window, err := ptypes.Duration(t.Spec.Restart.Window)
 	if err != nil {
 		log.G(ctx).WithError(err).Error("invalid restart lookback window")
-		return restartInfo.totalRestarts < service.Spec.Restart.MaxAttempts
+		return restartInfo.totalRestarts < t.Spec.Restart.MaxAttempts
 	}
 	lookback := time.Now().Add(-window)
 
@@ -181,11 +187,11 @@ func (r *RestartSupervisor) shouldRestart(ctx context.Context, t *api.Task, serv
 		restartInfo.restartedInstances = nil
 	}
 
-	return numRestarts < service.Spec.Restart.MaxAttempts
+	return numRestarts < t.Spec.Restart.MaxAttempts
 }
 
-func (r *RestartSupervisor) recordRestartHistory(restartTask *api.Task, service *api.Service) {
-	if service.Spec.Restart == nil || service.Spec.Restart.MaxAttempts == 0 {
+func (r *RestartSupervisor) recordRestartHistory(restartTask *api.Task) {
+	if restartTask.Spec.Restart == nil || restartTask.Spec.Restart.MaxAttempts == 0 {
 		// No limit on the number of restarts, so no need to record
 		// history.
 		return
@@ -202,6 +208,7 @@ func (r *RestartSupervisor) recordRestartHistory(restartTask *api.Task, service 
 	if r.history[tuple] == nil {
 		r.history[tuple] = &instanceRestartInfo{}
 	}
+
 	restartInfo := r.history[tuple]
 	restartInfo.totalRestarts++
 
@@ -210,7 +217,7 @@ func (r *RestartSupervisor) recordRestartHistory(restartTask *api.Task, service 
 	}
 	r.historyByService[restartTask.ServiceID][tuple] = struct{}{}
 
-	if service.Spec.Restart.Window != nil && (service.Spec.Restart.Window.Seconds != 0 || service.Spec.Restart.Window.Nanos != 0) {
+	if restartTask.Spec.Restart.Window != nil && (restartTask.Spec.Restart.Window.Seconds != 0 || restartTask.Spec.Restart.Window.Nanos != 0) {
 		if restartInfo.restartedInstances == nil {
 			restartInfo.restartedInstances = list.New()
 		}
@@ -229,7 +236,7 @@ func (r *RestartSupervisor) recordRestartHistory(restartTask *api.Task, service 
 // It must be called during an Update transaction to ensure that it does not
 // miss events. The purpose of the store.Tx argument is to avoid accidental
 // calls outside an Update transaction.
-func (r *RestartSupervisor) DelayStart(ctx context.Context, _ store.Tx, service *api.Service, oldTask *api.Task, newTaskID string, delay time.Duration, waitStop bool) <-chan struct{} {
+func (r *RestartSupervisor) DelayStart(ctx context.Context, _ store.Tx, oldTask *api.Task, newTaskID string, delay time.Duration, waitStop bool) <-chan struct{} {
 	ctx, cancel := context.WithCancel(context.Background())
 	doneCh := make(chan struct{})
 
@@ -282,24 +289,7 @@ func (r *RestartSupervisor) DelayStart(ctx context.Context, _ store.Tx, service 
 			close(doneCh)
 		}()
 
-		var oldTaskTimeout <-chan time.Time
-		if watch != nil {
-			container := service.Spec.GetContainer()
-			if container != nil {
-				gracePeriod, err := ptypes.Duration(&container.StopGracePeriod)
-				if err != nil {
-					log.G(ctx).WithError(err).Error("invalid stop grace period")
-					oldTaskTimeout = time.After(defaultOldTaskTimeout)
-				} else {
-					// Add a little time to the grace period
-					// because there may be latency between
-					// the agent and us.
-					oldTaskTimeout = time.After(gracePeriod + 5*time.Second)
-				}
-			} else {
-				oldTaskTimeout = time.After(defaultOldTaskTimeout)
-			}
-		}
+		oldTaskTimeout := time.After(r.taskTimeout)
 
 		// Wait for the delay to elapse, if one is specified.
 		if delay != 0 {

manager/orchestrator/updater_test.go
@@ -65,16 +65,18 @@ func TestUpdater(t *testing.T) {
 			Annotations: api.Annotations{
 				Name: "name1",
 			},
-			RuntimeSpec: &api.ServiceSpec_Container{
-				Container: &api.ContainerSpec{
-					Image: "v:1",
-					// This won't apply in this test because we set the old tasks to DEAD.
-					StopGracePeriod: *ptypes.DurationProto(time.Hour),
-				},
-			},
 			Mode: &api.ServiceSpec_Replicated{
 				Replicated: &api.ReplicatedService{
 					Instances: uint64(instances),
+				},
+			},
+			Task: api.TaskSpec{
+				Runtime: &api.TaskSpec_Container{
+					Container: &api.ContainerSpec{
+						Image: "v:1",
+						// This won't apply in this test because we set the old tasks to DEAD.
+						StopGracePeriod: *ptypes.DurationProto(time.Hour),
+					},
 				},
 			},
 		},
@@ -91,18 +93,18 @@ func TestUpdater(t *testing.T) {
 
 	originalTasks := getRunnableServiceTasks(t, s, service)
 	for _, task := range originalTasks {
-		assert.Equal(t, "v:1", task.GetContainer().Spec.Image)
+		assert.Equal(t, "v:1", task.Spec.GetContainer().Image)
 	}
 
-	service.Spec.GetContainer().Image = "v:2"
+	service.Spec.Task.GetContainer().Image = "v:2"
 	updater := NewUpdater(s, NewRestartSupervisor(s))
 	updater.Run(ctx, service, getRunnableServiceTasks(t, s, service))
 	updatedTasks := getRunnableServiceTasks(t, s, service)
 	for _, task := range updatedTasks {
-		assert.Equal(t, "v:2", task.GetContainer().Spec.Image)
+		assert.Equal(t, "v:2", task.Spec.GetContainer().Image)
 	}
 
-	service.Spec.GetContainer().Image = "v:3"
+	service.Spec.Task.GetContainer().Image = "v:3"
 	service.Spec.Update = &api.UpdateConfig{
 		Parallelism: 1,
 	}
@@ -110,10 +112,10 @@ func TestUpdater(t *testing.T) {
 	updater.Run(ctx, service, getRunnableServiceTasks(t, s, service))
 	updatedTasks = getRunnableServiceTasks(t, s, service)
 	for _, task := range updatedTasks {
-		assert.Equal(t, "v:3", task.GetContainer().Spec.Image)
+		assert.Equal(t, "v:3", task.Spec.GetContainer().Image)
 	}
 
-	service.Spec.GetContainer().Image = "v:4"
+	service.Spec.Task.GetContainer().Image = "v:4"
 	service.Spec.Update = &api.UpdateConfig{
 		Parallelism: 1,
 		Delay:       *ptypes.DurationProto(10 * time.Millisecond),
@@ -122,7 +124,7 @@ func TestUpdater(t *testing.T) {
 	updater.Run(ctx, service, getRunnableServiceTasks(t, s, service))
 	updatedTasks = getRunnableServiceTasks(t, s, service)
 	for _, task := range updatedTasks {
-		assert.Equal(t, "v:4", task.GetContainer().Spec.Image)
+		assert.Equal(t, "v:4", task.Spec.GetContainer().Image)
 	}
 }
 
@@ -161,10 +163,12 @@ func TestUpdaterStopGracePeriod(t *testing.T) {
 			Annotations: api.Annotations{
 				Name: "name1",
 			},
-			RuntimeSpec: &api.ServiceSpec_Container{
-				Container: &api.ContainerSpec{
-					Image:           "v:1",
-					StopGracePeriod: *ptypes.DurationProto(100 * time.Millisecond),
+			Task: api.TaskSpec{
+				Runtime: &api.TaskSpec_Container{
+					Container: &api.ContainerSpec{
+						Image:           "v:1",
+						StopGracePeriod: *ptypes.DurationProto(100 * time.Millisecond),
+					},
 				},
 			},
 			Mode: &api.ServiceSpec_Replicated{
@@ -188,17 +192,19 @@ func TestUpdaterStopGracePeriod(t *testing.T) {
 
 	originalTasks := getRunnableServiceTasks(t, s, service)
 	for _, task := range originalTasks {
-		assert.Equal(t, "v:1", task.GetContainer().Spec.Image)
+		assert.Equal(t, "v:1", task.Spec.GetContainer().Image)
 	}
 
 	before := time.Now()
 
-	service.Spec.GetContainer().Image = "v:2"
+	service.Spec.Task.GetContainer().Image = "v:2"
 	updater := NewUpdater(s, NewRestartSupervisor(s))
+	// Override the default (1 minute) to speed up the test.
+	updater.restarts.taskTimeout = 100 * time.Millisecond
 	updater.Run(ctx, service, getRunnableServiceTasks(t, s, service))
 	updatedTasks := getRunnableServiceTasks(t, s, service)
 	for _, task := range updatedTasks {
-		assert.Equal(t, "v:2", task.GetContainer().Spec.Image)
+		assert.Equal(t, "v:2", task.Spec.GetContainer().Image)
 	}
 
 	after := time.Now()

agent/exec/container/adapter.go
@@ -196,7 +196,7 @@ func (c *containerAdapter) remove(ctx context.Context) error {
 
 func (c *containerAdapter) createVolumes(ctx context.Context, client engineapi.APIClient) error {
 	// Create plugin volumes that are embedded inside a Mount
-	for _, mount := range c.container.task.GetContainer().Spec.Mounts {
+	for _, mount := range c.container.task.Spec.GetContainer().Mounts {
 		if mount.Template != nil {
 			req := c.container.volumeCreateRequest(mount)
 

agent/exec/container/container.go
@@ -42,12 +42,12 @@ func newContainerConfig(t *api.Task) (*containerConfig, error) {
 }
 
 func (c *containerConfig) setTask(t *api.Task) error {
-	container := t.GetContainer()
+	container := t.Spec.GetContainer()
 	if container == nil {
 		return exec.ErrRuntimeUnsupported
 	}
 
-	if container.Spec.Image == "" {
+	if container.Image == "" {
 		return ErrImageRequired
 	}
 
@@ -66,13 +66,13 @@ func (c *containerConfig) endpoint() *api.Endpoint {
 }
 
 func (c *containerConfig) spec() *api.ContainerSpec {
-	return &c.task.GetContainer().Spec
+	return c.task.Spec.GetContainer()
 }
 
 func (c *containerConfig) name() string {
-	if container := c.task.GetContainer(); container != nil && container.Annotations.Name != "" {
+	if c.task.Annotations.Name != "" {
 		// if set, use the container Annotations.Name field, set in the orchestrator.
-		return container.Annotations.Name
+		return c.task.Annotations.Name
 	}
 
 	// fallback to service.instance.id.
@@ -154,12 +154,10 @@ func (c *containerConfig) labels() map[string]string {
 		labels[k] = v
 	}
 
-	if container := c.task.GetContainer(); container != nil {
-		// we then apply the overrides from container, which may be set via the
-		// orchestrator.
-		for k, v := range container.Annotations.Labels {
-			labels[k] = v
-		}
+	// we then apply the overrides from the task, which may be set via the
+	// orchestrator.
+	for k, v := range c.task.Annotations.Labels {
+		labels[k] = v
 	}
 
 	// finally, we apply the system labels, which override all labels.
@@ -232,7 +230,7 @@ func (c *containerConfig) resources() enginecontainer.Resources {
 	//
 	// TODO(aluzzardi): We might want to set some limits anyway otherwise
 	// "unlimited" tasks will step over the reservation of other tasks.
-	r := c.spec().Resources
+	r := c.task.Spec.Resources
 	if r == nil || r.Limits == nil {
 		return resources
 	}
@@ -271,13 +269,8 @@ func (c *containerConfig) virtualIP(networkID string) string {
 }
 
 func (c *containerConfig) networkingConfig() *network.NetworkingConfig {
-	var networks []*api.NetworkAttachment
-	if c.task.GetContainer() != nil {
-		networks = c.task.Networks
-	}
-
 	epConfig := make(map[string]*network.EndpointSettings)
-	for _, na := range networks {
+	for _, na := range c.task.Networks {
 		var ipv4, ipv6 string
 		for _, addr := range na.Addresses {
 			ip, _, err := net.ParseCIDR(addr)

agent/exec/container/controller_integration_test.go
@@ -44,9 +44,9 @@ func TestControllerFlowIntegration(t *testing.T) {
 		ID:        "dockerexec-integration-task-id",
 		ServiceID: "dockerexec-integration-service-id",
 		NodeID:    "dockerexec-integration-node-id",
-		Runtime: &api.Task_Container{
-			Container: &api.Container{
-				Spec: api.ContainerSpec{
+		Spec: api.TaskSpec{
+			Runtime: &api.TaskSpec_Container{
+				Container: &api.ContainerSpec{
 					Command: []string{"sh", "-c", "sleep 5"},
 					Image:   "alpine",
 				},

agent/exec/container/controller_test.go
@@ -302,9 +302,9 @@ func genTask(t *testing.T) *api.Task {
 		ID:        identity.NewID(),
 		ServiceID: serviceID,
 		NodeID:    nodeID,
-		Runtime: &api.Task_Container{
-			Container: &api.Container{
-				Spec: api.ContainerSpec{
+		Spec: api.TaskSpec{
+			Runtime: &api.TaskSpec_Container{
+				Container: &api.ContainerSpec{
 					Image:           reference,
 					StopGracePeriod: *ptypes.DurationProto(10 * time.Second),
 				},

api/objects.proto
@@ -101,46 +101,54 @@ message Task {
 
 	Meta meta = 2 [(gogoproto.nullable) = false];
 
+	// Spec defines the desired state of the task as specified by the user.
+	// The system will honor this and will *never* modify it.
+	TaskSpec spec = 3 [(gogoproto.nullable) = false];
+
 	// ServiceID indicates the service under which this task is orchestrated. This
 	// should almost always be set.
-	string service_id = 3 [(gogoproto.customname) = "ServiceID"];
+	string service_id = 4 [(gogoproto.customname) = "ServiceID"];
 
 	// Instance is the instance number for a task. For example, if a
 	// replicated service has instances = 2, there will be a task with
 	// instance = 1, and another with instance = 2.
-	uint64 instance = 4;
+	uint64 instance = 5;
 
 	// NodeID indicates the node to which the task is assigned. If this field
 	// is empty or not set, the task is unassigned.
-	string node_id = 5 [(gogoproto.customname) = "NodeID"];
+	string node_id = 6 [(gogoproto.customname) = "NodeID"];
+
+	// Annotations defines the names and labels for the runtime, as set by
+	// the cluster manager.
+	//
+	// As backup, if this field has an empty name, the runtime will
+	// allocate a unique name for the actual container.
+	//
+	// NOTE(stevvooe): The preserves the ability for us to making naming
+	// decisions for tasks in orchestrator, albeit, this is left empty for now.
+	Annotations annotations = 7 [(gogoproto.nullable) = false];
 
 	// ServiceAnnotations is a direct copy of the service name and labels when
 	// this task is created.
 	//
-	// Labels set here will not be propagated to the runtime target, such as a
+	// Labels set here will *not* be propagated to the runtime target, such as a
 	// container. Use labels on the runtime target for that purpose.
-	Annotations service_annotations = 6 [(gogoproto.nullable) = false];
+	Annotations service_annotations = 8 [(gogoproto.nullable) = false];
 
-	// Task carries the runtime information which by itself carries the
-	// spec for the runtime. For e.g. Container contains ContainerSpec.
-	oneof runtime {
-		Container container = 7;
-	}
-
-	TaskStatus status = 8 [(gogoproto.nullable) = false];
+	TaskStatus status = 9 [(gogoproto.nullable) = false];
 
 	// DesiredState is the target state for the task. It is set to
 	// TaskStateRunning when a task is first created, and changed to
 	// TaskStateShutdown if the manager wants to terminate the task. This field
 	// is only written by the manager.
-	TaskState desired_state = 9;
+	TaskState desired_state = 10;
 
 	// List of network attachments by the task.
-	repeated NetworkAttachment networks = 10;
+	repeated NetworkAttachment networks = 11;
 
 	// A copy of runtime state of service endpoint from Service
 	// object to be distributed to agents as part of the task.
-	Endpoint endpoint = 11;
+	Endpoint endpoint = 12;
 }
 
 // NetworkAttachment specifies the network parameters of attachment to
@@ -154,23 +162,6 @@ message NetworkAttachment {
 	// List of IPv4/IPv6 addresses that are assigned to the object
 	// as part of getting attached to this network.
 	repeated string addresses = 2;
-}
-
-// Container contains all runtime state of a task which is container runtime specific.
-message Container {
-	// Annotations defines the names and labels for the container, as set by
-	// the cluster manager. Any name and labels specified here will supercede
-	// those defined in the spec.
-	//
-	// As backup, if this field has an empty name, the container runtime will
-	// allocate a unique name for the actual container.
-	//
-	// NOTE(stevvooe): The preserves the ability for us to making naming
-	// decisions for tasks in orchestrator, albeit, this is left empty for now.
-	Annotations annotations = 1 [(gogoproto.nullable) = false];
-
-	// Spec defines the container specific configuration for this container.
-	ContainerSpec spec = 2 [(gogoproto.nullable) = false];
 }
 
 message Network {
@@ -200,7 +191,7 @@ message Cluster {
 	RootCA root_ca = 4 [(gogoproto.customname) = "RootCA"];
 
 	// Symmetric encryption key distributed by the lead manager. Used by agents
-	// for securing network bootstrapping and communication. 
+	// for securing network bootstrapping and communication.
 	repeated EncryptionKey network_bootstrap_keys = 5;
 
 	// Logical clock used to timestamp every key.  It allows other managers

commit 6eebd6019b0aafe91eda1833ab9c0a65114ce9a5

orchestrator: respect task stop grace period in stop-first updates (#3274)
when doing a stop-first service update, DelayStart was waiting on
r.TaskTimeout (defaults to 1m) to give up on the old task. if the
container had a StopGracePeriod longer then 1m, this timer expired while
the old container was still shutting down and relased the new task
too early, so both containers ended up running at the same time.

this was a regression from 47ddece4 where service wasn't passed to DelayStart
anymore. but since StopGracePeriod is already in oldTask.Spec, we can just
read it directly from the container spec without needing the service object,
and use max(TaskTimeout, grace + 5s buffer).

fixes #3274

Signed-off-by: ADİL ALPEREN ÇİFTCİ <134228585+adilalperenciftci@users.noreply.github.com>

manager/orchestrator/restart/restart.go
@@ -19,6 +19,25 @@ import (
 
 const defaultOldTaskTimeout = time.Minute
 
+// buffer added to stop grace period to account for SIGKILL and state reporting
+const stopGracePeriodBuffer = 5 * time.Second
+
+func stopGracePeriod(ctx context.Context, t *api.Task) (time.Duration, bool) {
+	container := t.Spec.GetContainer()
+	if container == nil || container.StopGracePeriod == nil {
+		return 0, false
+	}
+	grace, err := gogotypes.DurationFromProto(container.StopGracePeriod)
+	if err != nil {
+		log.G(ctx).WithError(err).WithField("task.id", t.ID).Error("invalid stop grace period")
+		return 0, false
+	}
+	if grace <= 0 {
+		return 0, false
+	}
+	return grace, true
+}
+
 type restartedInstance struct {
 	timestamp time.Time
 }
@@ -485,7 +504,18 @@ func (r *Supervisor) DelayStart(ctx context.Context, _ store.Tx, oldTask *api.Ta
 			close(doneCh)
 		}()
 
-		oldTaskTimer := time.NewTimer(r.TaskTimeout)
+		// if task specifies a StopGracePeriod larger than TaskTimeout, wait for it
+		// so stop-first doesn't start the new task before the old one stops.
+		oldTaskTimeout := r.TaskTimeout
+		if waitForTask {
+			if grace, ok := stopGracePeriod(ctx, oldTask); ok {
+				if budget := grace + stopGracePeriodBuffer; budget > oldTaskTimeout {
+					oldTaskTimeout = budget
+				}
+			}
+		}
+
+		oldTaskTimer := time.NewTimer(oldTaskTimeout)
 		defer oldTaskTimer.Stop()
 
 		// Wait for the delay to elapse, if one is specified.

manager/orchestrator/update/updater_test.go
@@ -702,3 +702,128 @@ func TestUpdaterOrder(t *testing.T) {
 		}
 	}
 }
+
+// TestUpdaterStopGracePeriod tests that stop-first updates respect the old task's
+// stop grace period before releasing the replacement (#3274).
+func TestUpdaterStopGracePeriod(t *testing.T) {
+	ctx := context.Background()
+	s := store.NewMemoryStore(nil)
+	assert.NotNil(t, s)
+	defer s.Close()
+
+	// simulate slow shutdown: don't progress old task to shutdown yet
+	watch, cancel := state.Watch(s.WatchQueue(), api.EventUpdateTask{})
+	defer cancel()
+	go func() {
+		for e := range watch {
+			task := e.(api.EventUpdateTask).Task
+			_ = s.Update(func(tx store.Tx) error {
+				task = store.GetTask(tx, task.ID)
+				if task == nil {
+					return nil
+				}
+				if task.DesiredState == api.TaskStateRunning && task.Status.State != api.TaskStateRunning {
+					task.Status.State = api.TaskStateRunning
+					return store.UpdateTask(tx, task)
+				}
+				return nil
+			})
+		}
+	}()
+
+	const (
+		stopGracePeriod = 2 * time.Second
+		taskTimeout     = 50 * time.Millisecond
+		observeAfter    = 500 * time.Millisecond
+	)
+
+	service := &api.Service{
+		ID: "id1",
+		Spec: api.ServiceSpec{
+			Annotations: api.Annotations{
+				Name: "name1",
+			},
+			Task: api.TaskSpec{
+				Runtime: &api.TaskSpec_Container{
+					Container: &api.ContainerSpec{
+						Image:           "v:1",
+						StopGracePeriod: gogotypes.DurationProto(stopGracePeriod),
+					},
+				},
+			},
+			Mode: &api.ServiceSpec_Replicated{
+				Replicated: &api.ReplicatedService{
+					Replicas: 1,
+				},
+			},
+			Update: &api.UpdateConfig{
+				Order:   api.UpdateConfig_STOP_FIRST,
+				Monitor: gogotypes.DurationProto(50 * time.Millisecond),
+			},
+		},
+	}
+
+	err := s.Update(func(tx store.Tx) error {
+		assert.NoError(t, store.CreateService(tx, service))
+		task := orchestrator.NewTask(nil, service, 0, "")
+		task.Status.State = api.TaskStateRunning
+		assert.NoError(t, store.CreateTask(tx, task))
+		return nil
+	})
+	assert.NoError(t, err)
+
+	originalSlots := getRunnableSlotSlice(t, s, service)
+	require.Len(t, originalSlots, 1)
+	require.Len(t, originalSlots[0], 1)
+	oldTaskID := originalSlots[0][0].ID
+
+	service.Spec.Task.GetContainer().Image = "v:2"
+	updater := NewUpdater(s, restart.NewSupervisor(s), nil, service)
+	updater.restarts.TaskTimeout = taskTimeout
+
+	runDone := make(chan struct{})
+	go func() {
+		defer close(runDone)
+		updater.Run(ctx, originalSlots)
+	}()
+
+	// replacement should still be in READY since grace period has not elapsed
+	time.Sleep(observeAfter)
+
+	var replacement *api.Task
+	s.View(func(tx store.ReadTx) {
+		tasks, err := store.FindTasks(tx, store.ByServiceID(service.ID))
+		require.NoError(t, err)
+		for _, task := range tasks {
+			if task.ID != oldTaskID {
+				replacement = task
+			}
+		}
+	})
+	require.NotNil(t, replacement, "updater should have created a replacement task")
+	assert.Equal(t, "v:2", replacement.Spec.GetContainer().Image)
+	assert.Equal(t, api.TaskStateReady, replacement.DesiredState)
+
+	// finish stopping old task; updater should proceed immediately
+	err = s.Update(func(tx store.Tx) error {
+		task := store.GetTask(tx, oldTaskID)
+		require.NotNil(t, task)
+		task.Status.State = api.TaskStateShutdown
+		return store.UpdateTask(tx, task)
+	})
+	assert.NoError(t, err)
+
+	select {
+	case <-runDone:
+	case <-time.After(stopGracePeriod):
+		t.Fatal("updater did not proceed after the old task stopped")
+	}
+
+	updatedSlots := getRunnableSlotSlice(t, s, service)
+	require.Len(t, updatedSlots, 1)
+	for _, slot := range updatedSlots {
+		for _, task := range slot {
+			assert.Equal(t, "v:2", task.Spec.GetContainer().Image)
+		}
+	}
+}